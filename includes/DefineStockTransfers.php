1347761fe8e719ce64b576c8aff796a68a6602cc
daintree <>
2004-06-17 08:15:50 +0000

    Stock Transfer class

---
+++ includes/DefineStockTransfers.php
@@ -0,0 +1,60 @@
+<?php
+/* $Revision: 1.1 $ */
+
+/*Class to hold stock transfer records */
+
+class StockTransfer {
+
+	Var $TrfID;
+	Var $StockLocationFrom;
+	Var $StockLocationFromName;
+	Var $StockLocationTo; /*Used in stock transfers only */
+	Var $StockLocationToName;
+	Var $TranDate; /*Used in stock transfers only */
+	Var $TransferItem; /*Array of LineItems */
+
+	function StockTransfer($TrfID, $StockLocationFrom, $StockLocationFromName, $StockLocationTo, $StockLocationToName, $TranDate){
+		$this->TrfID = $TrfID;
+		$this->StockLocationFrom = $StockLocationFrom;
+		$this->StockLocationFromName = $StockLocationFromName;
+		$this->StockLocationTo =$StockLocationTo;
+		$this->StockLocationToName =$StockLocationToName;
+		$this->TranDate = $TranDate;
+		$this->TransferItem=array(); /*Array of LineItem s */
+	}
+}
+
+class LineItem {
+	var $StockID;
+	var $ItemDescription;
+	Var $ShipQty;
+	Var $Quantity;
+	Var $PartUnit;
+	var $Controlled;
+	var $Serialised;
+	Var $DecimalPlaces;
+	var $SerialItems; /*array to hold controlled items*/
+//Constructor
+	function LineItem($StockID,
+			$ItemDescription,
+			$Quantity,
+			$PartUnit,
+			$Controlled,
+			$Serialised,
+			$DecimalPlaces){
+
+		$this->StockID = $StockID;
+		$this->ItemDescription = $ItemDescription;
+		$this->PartUnit = $PartUnit;
+		$this->Controlled = $Controlled;
+		$this->Serialised = $Serialised;
+		$this->ShipQty = $Quantity;
+		if ($this->Controlled==1){
+			$this->Quantity = 0;
+		} else {
+			$this->Quantity = $Quantity;
+		}
+		$this->SerialItems = array();
+	}
+}
+?>
