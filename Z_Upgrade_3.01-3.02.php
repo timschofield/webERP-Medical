c04e494ba9d64db791d24ebdb7a6624d0384aeff
daintree <>
2006-02-01 09:15:25 +0000

    Upgrade 3.01-3.02 made check if run already and exit nicely as suggested by Muz

--- Z_Upgrade_3.01-3.02.php
+++ Z_Upgrade_3.01-3.02.php
@@ -2,6 +2,18 @@
 
 $PageSecurity = 15;
 include('includes/session.inc');
+$title = _('Upgrade webERP 3.01 - 3.02');
+include('includes/header.inc');
+
+prnMsg(_('Upgrade script to number salesorderdetails records as required by version 3.02 .... please wait'),'info');
+
+$TestAlreadyDoneResult = DB_query('SELECT * FROM salesorderdetails WHERE orderlineno>=1',$db);
+if (DB_num_rows($TestAlreadyDoneResult)>0){
+	prnMsg(_('The upgrade script appears to have been run already successfully - there is no need to re-run it'),'info');
+	include('includes/footer.inc');
+	exit;
+}
+
 
 $lineno = 1;
 $orderno = 0;
@@ -27,5 +39,5 @@ while ($SalesOrderDetails = DB_fetch_array($SalesOrdersResult)) {
 DB_query( 'ALTER TABLE salesorderdetails ADD CONSTRAINT salesorderdetails_pk primary key(orderno, orderlineno)',$db);
 
 prnMsg(_('The sales orderdetails lines have been numbered appropriately for version 3.02','success'));
-
+include('includes/footer.inc');
 ?>
