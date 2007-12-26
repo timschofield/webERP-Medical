f4788e83d83974d1aff6f0d4a7dbdfcfa670277e
tim_schofield <>
2007-12-26 11:17:35 +0000

    Database changes for factoring companies

--- sql/mysql/upgrade3.08-3.09.sql
+++ sql/mysql/upgrade3.08-3.09.sql
@@ -8,3 +8,25 @@ CREATE TABLE `audittrail` (
 
 ALTER TABLE `salesorders` CHANGE `contactemail` `contactemail` VARCHAR( 40 ) DEFAULT NULL;
 INSERT INTO `config` ( `confname` , `confvalue` ) VALUES ('MonthsAuditTrail', '1');
+
+CREATE TABLE `factorcompanies` (
+  `id` int(11) NOT NULL auto_increment,
+  `coyname` varchar(50) NOT NULL default '',
+  `address1` varchar(40) NOT NULL default '',
+  `address2` varchar(40) NOT NULL default '',
+  `address3` varchar(40) NOT NULL default '',
+  `address4` varchar(40) NOT NULL default '',
+  `address5` varchar(20) NOT NULL default '',
+  `address6` varchar(15) NOT NULL default '',
+  `contact` varchar(25) NOT NULL default '',
+  `telephone` varchar(25) NOT NULL default '',
+  `fax` varchar(25) NOT NULL default '',
+  `email` varchar(55) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `factorcompanies` ( `id` , `coyname` ) VALUES (null, 'None');
+
+ALTER TABLE `suppliers` ADD COLUMN  `factorcompanyid` int(11) NOT NULL default 1 AFTER `taxgroupid`;
+ALTER TABLE `suppliers` ADD CONSTRAINT `suppliers_ibfk_4` FOREIGN KEY (`factorcompanyid`) REFERENCES `factorcompanies` (`id`);
+
