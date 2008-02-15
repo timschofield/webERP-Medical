3081cc72a4c74ae03bcd4b37f0c7285c7153d664
tim_schofield <>
2008-02-15 19:20:45 +0000

    Update for 3.09

--- sql/mysql/upgrade3.08-3.09.sql
+++ sql/mysql/upgrade3.08-3.09.sql
@@ -31,9 +31,9 @@ ALTER TABLE `suppliers` ADD COLUMN  `factorcompanyid` int(11) NOT NULL default 1
 ALTER TABLE `suppliers` ADD CONSTRAINT `suppliers_ibfk_4` FOREIGN KEY (`factorcompanyid`) REFERENCES `factorcompanies` (`id`);
 
 ALTER TABLE `stockmaster` ADD COLUMN  `perishable` tinyint(1) NOT NULL default 0 AFTER `serialised`;
-ALTER TABLE `stockmaster` ADD `appendfile` varchar(40) NOT NULL default 'none' AFTER `serialised`;
+ALTER TABLE `stockmaster` ADD COLUMN `appendfile` varchar(40) NOT NULL default 'none' AFTER `serialised`;
 ALTER TABLE `stockserialitems` ADD COLUMN  `expirationdate` datetime NOT NULL default '0000-00-00' AFTER `serialno`;
-ALTER TABLE `bankaccounts` ADD `currcode` CHAR( 3 ) NOT NULL AFTER `accountcode` ;
+ALTER TABLE `bankaccounts` ADD COLUMN `currcode` CHAR( 3 ) NOT NULL AFTER `accountcode` ;
 ALTER TABLE `bankaccounts` ADD INDEX ( `currcode` ) ;
 ALTER TABLE `banktrans` CHANGE `exrate` `exrate` DOUBLE NOT NULL DEFAULT '1' COMMENT 'From bank account currency to payment currency';
 ALTER TABLE `banktrans` ADD `functionalexrate` DOUBLE NOT NULL DEFAULT '1' COMMENT 'Account currency to functional currency';
