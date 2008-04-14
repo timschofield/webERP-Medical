76862b3c7e35d56b8069c04a5ca42f3b5065fe80
tim_schofield <>
2008-04-14 20:25:04 +0000

    Add currency searches to api

---
+++ api/api_currencies.php
@@ -0,0 +1,32 @@
+<?php
+
+	function GetCurrencyList($user, $password) {
+		$Errors = array();
+		$db = db($user, $password);
+		if (gettype($db)=='integer') {
+			$Errors[0]=NoAuthorisation;
+			return $Errors;
+		}
+		$sql = 'SELECT currabrev FROM currencies';
+		$result = DB_query($sql, $db);
+		$i=0;
+		while ($myrow=DB_fetch_array($result)) {
+			$CurrencyList[$i]=$myrow[0];
+			$i++;
+		}
+		return $CurrencyList;
+	}
+	
+	function GetCurrencyDetails($currency, $user, $password) {
+		$Errors = array();
+		$db = db($user, $password);
+		if (gettype($db)=='integer') {
+			$Errors[0]=NoAuthorisation;
+			return $Errors;
+		}
+		$sql = 'SELECT * FROM currencies WHERE currabrev="'.$currency.'"';
+		$result = DB_query($sql, $db);
+		return DB_fetch_array($result)
+	}
+
+?>
