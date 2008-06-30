ca2ef76680e0c370c19c8d61ce1e5b219d54a888
emdeex <>
2008-06-30 01:33:26 +0000

    Modified thes DB values for flexibility, longer notes, longer role, phone can be varchar.

--- sql/mysql/upgrade3.09-3.10.sql
+++ sql/mysql/upgrade3.09-3.10.sql
@@ -2,8 +2,8 @@ CREATE TABLE `custcontacts` (
   `contid` int(11) NOT NULL auto_increment,
   `debtorno` varchar(10) NOT NULL,
   `contactname` varchar(40) NOT NULL,
-  `role` varchar(10) NOT NULL,
-  `phoneno` int(10) NOT NULL,
-  `notes` varchar(40) NOT NULL,
+  `role` varchar(40) NOT NULL,
+  `phoneno` varchar(20) NOT NULL,
+  `notes` varchar(255) NOT NULL,
   PRIMARY KEY  (`contid`)
 ) ENGINE=InnoDB;
