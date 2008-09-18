835eee9c96da946ffd4ca5c2e9b7004813b1cd8a
tim_schofield <>
2008-09-18 21:42:55 +0000

    Script to give details of a database table

---
+++ Z_DescribeTable.php
@@ -0,0 +1,27 @@
+<?php
+
+$PageSecurity = 11;
+
+include('includes/session.inc');
+$title = _('Database table details');
+include('includes/header.inc');
+
+$sql='DESCRIBE '.$_GET['table'];
+$result=DB_query($sql, $db);
+
+echo '<center><table><tr>';
+echo '<th>'._('Field name').'</th>';
+echo '<th>'._('Field type').'</th>';
+echo '<th>'._('Can field be null').'</th>';
+echo '<th>'._('Default').'</th>';
+while ($myrow=DB_fetch_row($result)) {
+	echo '<tr><td>' .$myrow[0] .'</td><td>';
+	echo $myrow[1] .'</td><td>';
+	echo $myrow[2] .'</td><td>';
+	echo $myrow[4] .'</td></tr>';
+}
+
+include('includes/footer.inc');
+
+
+?>
