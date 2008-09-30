0039efdcfd9ac3211d9d7bbe3b7b4095ac9c29db
tim_schofield <>
2008-09-30 08:01:18 +0000

    Correct bug in credit note transaction

--- api/api_debtortransactions.php
+++ api/api_debtortransactions.php
@@ -3,7 +3,7 @@
 /* Check that the transaction number is unique
  * for this type of transaction*/
 	function VerifyTransNo($TransNo, $Type, $i, $Errors, $db) {
-		$Searchsql = "SELECT count(transno) 
+		$Searchsql = "SELECT count(transno)
 				FROM debtortrans
 				WHERE type=".$Type." and transno=".$TransNo;
 		$SearchResult=DB_query($Searchsql, $db);
@@ -11,14 +11,14 @@
 		if ($answer[0]>0) {
 			$Errors[$i] = TransactionNumberAlreadyExists;
 		}
-		return $Errors;		
+		return $Errors;
 	}
 
 function ConvertToSQLDate($DateEntry) {
 
 //for MySQL dates are in the format YYYY-mm-dd
 
-	
+
 	if (strpos($DateEntry,'/')) {
 		$Date_Array = explode('/',$DateEntry);
 	} elseif (strpos ($DateEntry,'-')) {
@@ -39,8 +39,8 @@ function ConvertToSQLDate($DateEntry) {
 	}
 
 } // end function ConvertSQLDate
-	
-/* Check that the transaction date is a valid date. The date 
+
+/* Check that the transaction date is a valid date. The date
  * must be in the same format as the date format specified in the
  * target webERP company */
 	function VerifyTransactionDate($TranDate, $i, $Errors, $db) {
@@ -69,7 +69,7 @@ function ConvertToSQLDate($DateEntry) {
 		}
 		return $Errors;
 	}
-	
+
 /* Find the period number from the transaction date */
 	function GetPeriodFromTransactionDate($TranDate, $i, $Errors, $db) {
 		$sql='select confvalue from config where confname="'.DefaultDateFormat.'"';
@@ -102,17 +102,17 @@ function ConvertToSQLDate($DateEntry) {
 		$myrow=DB_fetch_array($result);
 		return $myrow[0];
 	}
-	
+
 /* Verify that the Settled flag is a 1 or 0 */
 	function VerifySettled($Settled, $i, $Errors) {
 		if ($Settled!=0 and $Settled!=1) {
-			$Errors[$i] = InvalidSettled;			
+			$Errors[$i] = InvalidSettled;
 		}
 		return $Errors;
 	}
 
 /* Check that the transaction reference is 20 characters
- *  or less long */		
+ *  or less long */
 	function VerifyReference($reference, $i, $Errors) {
 		if (strlen($reference)>20) {
 			$Errors[$i] = IncorrectReference;
@@ -120,79 +120,79 @@ function ConvertToSQLDate($DateEntry) {
 		return $Errors;
 	}
 
-/* Check that the tpe field is 2 characters or less long */		
+/* Check that the tpe field is 2 characters or less long */
 	function VerifyTpe($tpe, $i, $Errors) {
 		if (strlen($tpe)>20) {
 			$Errors[$i] = IncorrectTpe;
 		}
 		return $Errors;
 	}
-	
-/* Verify that the order number is numeric */	
+
+/* Verify that the order number is numeric */
 	function VerifyOrderNumber($order, $i, $Errors) {
 		if (!is_numeric($order)) {
-			$Errors[$i] = InvalidOrderNumbers;			
-		}
-		return $Errors;
-	}
-	
-/* Verify that the exchange rate is numeric */	
-	function VerifyExchangeRate($rate, $i, $Errors) {
-		if (!is_numeric($rate)) {
-			$Errors[$i] = InvalidExchangeRate;			
-		}
-		return $Errors;
-	}
-	
-/* Verify that the ovamount is numeric */	
-	function VerifyOVAmount($ovamount, $i, $Errors) {
-		if (!is_numeric($ovamount)) {
-			$Errors[$i] = InvalidOVAmount;			
-		}
-		return $Errors;
-	}
-	
-/* Verify that the ovgst is numeric */	
-	function VerifyOVGst($ovgst, $i, $Errors) {
-		if (!is_numeric($ovgst)) {
-			$Errors[$i] = InvalidOVGst;			
-		}
-		return $Errors;
-	}
-	
-/* Verify that the ovfreight is numeric */	
-	function VerifyOVFreight($ovfreight, $i, $Errors) {
-		if (!is_numeric($ovfreight)) {
-			$Errors[$i] = InvalidOVFreight;			
-		}
-		return $Errors;
-	}
-	
-/* Verify that the ovdiscount is numeric */	
-	function VerifyOVDiscount($ovdiscount, $i, $Errors) {
-		if (!is_numeric($ovdiscount)) {
-			$Errors[$i] = InvalidOVDiscount;			
-		}
-		return $Errors;
-	}
-	
-/* Verify that the diffonexch is numeric */	
-	function VerifyDiffOnExchange($diffonexch, $i, $Errors) {
-		if (!is_numeric($diffonexch)) {
-			$Errors[$i] = InvalidDiffOnExchange;			
-		}
-		return $Errors;
-	}
-	
-/* Verify that the allocated figure is numeric */	
-	function VerifyAllocated($alloc, $i, $Errors) {
-		if (!is_numeric($alloc)) {
-			$Errors[$i] = InvalidAllocation;			
+			$Errors[$i] = InvalidOrderNumbers;
 		}
 		return $Errors;
 	}
 
-/* Check that the invoice text is 256 characters or less long */		
+/* Verify that the exchange rate is numeric */
+	function VerifyExchangeRate($rate, $i, $Errors) {
+		if (!is_numeric($rate)) {
+			$Errors[$i] = InvalidExchangeRate;
+		}
+		return $Errors;
+	}
+
+/* Verify that the ovamount is numeric */
+	function VerifyOVAmount($ovamount, $i, $Errors) {
+		if (!is_numeric($ovamount)) {
+			$Errors[$i] = InvalidOVAmount;
+		}
+		return $Errors;
+	}
+
+/* Verify that the ovgst is numeric */
+	function VerifyOVGst($ovgst, $i, $Errors) {
+		if (!is_numeric($ovgst)) {
+			$Errors[$i] = InvalidOVGst;
+		}
+		return $Errors;
+	}
+
+/* Verify that the ovfreight is numeric */
+	function VerifyOVFreight($ovfreight, $i, $Errors) {
+		if (!is_numeric($ovfreight)) {
+			$Errors[$i] = InvalidOVFreight;
+		}
+		return $Errors;
+	}
+
+/* Verify that the ovdiscount is numeric */
+	function VerifyOVDiscount($ovdiscount, $i, $Errors) {
+		if (!is_numeric($ovdiscount)) {
+			$Errors[$i] = InvalidOVDiscount;
+		}
+		return $Errors;
+	}
+
+/* Verify that the diffonexch is numeric */
+	function VerifyDiffOnExchange($diffonexch, $i, $Errors) {
+		if (!is_numeric($diffonexch)) {
+			$Errors[$i] = InvalidDiffOnExchange;
+		}
+		return $Errors;
+	}
+
+/* Verify that the allocated figure is numeric */
+	function VerifyAllocated($alloc, $i, $Errors) {
+		if (!is_numeric($alloc)) {
+			$Errors[$i] = InvalidAllocation;
+		}
+		return $Errors;
+	}
+
+/* Check that the invoice text is 256 characters or less long */
 	function VerifyInvoiceText($invtext, $i, $Errors) {
 		if (strlen($invtext)>256) {
 			$Errors[$i] = IncorrectInvoiceText;
@@ -200,23 +200,23 @@ function ConvertToSQLDate($DateEntry) {
 		return $Errors;
 	}
 
-/* Check that the ship via field is 10 characters or less long */		
+/* Check that the ship via field is 10 characters or less long */
 	function VerifyShipVia($shipvia, $i, $Errors) {
 		if (strlen($shipvia)>10) {
 			$Errors[$i] = InvalidShipVia;
 		}
 		return $Errors;
 	}
-	
+
 /* Verify that the edisent flag is a 1 or 0 */
 	function VerifyEdiSent($edisent, $i, $Errors) {
 		if ($edisent!=0 and $edisent!=1) {
-			$Errors[$i] = InvalidEdiSent;			
+			$Errors[$i] = InvalidEdiSent;
 		}
 		return $Errors;
 	}
 
-/* Check that the consignment field is 15 characters or less long */		
+/* Check that the consignment field is 15 characters or less long */
 	function VerifyConsignment($consignment, $i, $Errors) {
 		if (strlen($consignment)>15) {
 			$Errors[$i] = InvalidConsignment;
@@ -224,9 +224,9 @@ function ConvertToSQLDate($DateEntry) {
 		return $Errors;
 	}
 
-/* Retrieves the default sales GL code for a given part code and sales area */	
+/* Retrieves the default sales GL code for a given part code and sales area */
 	function GetSalesGLCode($salesarea, $partnumber, $db) {
-		$sql='select salesglcode from salesglpostings 
+		$sql='select salesglcode from salesglpostings
 			where stkcat=(select categoryid from stockmaster where stockid="'
 			.$partnumber.'") and id="'.$salesarea.'"';
 		$result=DB_query($sql, $db);
@@ -234,7 +234,7 @@ function ConvertToSQLDate($DateEntry) {
 		return $myrow[0];
 	}
 
-/* Retrieves the default debtors code for webERP */	
+/* Retrieves the default debtors code for webERP */
 	function GetDebtorsGLCode($db) {
 		$sql='select debtorsact from companies';
 		$result=DB_query($sql, $db);
@@ -242,7 +242,7 @@ function ConvertToSQLDate($DateEntry) {
 		return $myrow[0];
 	}
 
-/* Retrieves the next transaction number for the given type */	
+/* Retrieves the next transaction number for the given type */
 	function GetNextTransactionNo($type, $db) {
 		$sql='select typeno from systypes where typeid='.$type;
 		$result=DB_query($sql, $db);
@@ -251,7 +251,7 @@ function ConvertToSQLDate($DateEntry) {
 		return $NextTransaction;
 	}
 
-/* Create a customer invoice in webERP. This function will bypass the 
+/* Create a customer invoice in webERP. This function will bypass the
  * normal procedure in webERP for creating a sales order first, and then
  * delivering it.
  */
@@ -276,52 +276,51 @@ function ConvertToSQLDate($DateEntry) {
 		$Errors=VerifyBranchNoExists($InvoiceDetails['debtorno'],$InvoiceDetails['branchcode'], sizeof($Errors), $Errors, $db);
 		$Errors=VerifyTransNO($InvoiceDetails['transno'], 10, sizeof($Errors), $Errors, $db);
 		$Errors=VerifyTransactionDate($InvoiceDetails['trandate'], sizeof($Errors), $Errors, $db);
-//		$InvoiceDetails['prd']=GetPeriodFromTransactionDate($InvoiceDetails['trandate'], sizeof($Errors), $Errors, $db);
 		if (isset($InvoiceDetails['settled'])){
 			$Errors=VerifySettled($InvoiceDetails['settled'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['reference'])){
 			$Errors=VerifyReference($InvoiceDetails['reference'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['tpe'])){
 			$Errors=VerifyTpe($InvoiceDetails['tpe'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['order_'])){
 			$Errors=VerifyOrderNumber($InvoiceDetails['order_'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['rate'])){
 			$Errors=VerifyExchangeRate($InvoiceDetails['rate'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['ovamount'])){
 			$Errors=VerifyOVAmount($InvoiceDetails['ovamount'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['ovgst'])){
 			$Errors=VerifyOVGst($InvoiceDetails['ovgst'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['ovfreight'])){
 			$Errors=VerifyOVFreight($InvoiceDetails['ovfreight'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['ovdiscount'])){
 			$Errors=VerifyOVDiscount($InvoiceDetails['ovdiscount'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['diffonexch'])){
 			$Errors=VerifyDiffOnExchange($InvoiceDetails['diffonexch'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['alloc'])){
 			$Errors=VerifyAllocated($InvoiceDetails['alloc'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['invtext'])){
 			$Errors=VerifyInvoiceText($InvoiceDetails['invtext'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['shipvia'])){
 			$Errors=VerifyShipVia($InvoiceDetails['shipvia'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['edisent'])){
 			$Errors=VerifyEdiSent($InvoiceDetails['edisent'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($InvoiceDetails['consignment'])){
 			$Errors=VerifyConsignment($InvoiceDetails['consignment'], sizeof($Errors), $Errors);
-		}	
+		}
 		$FieldNames='';
 		$FieldValues='';
 		$InvoiceDetails['trandate']=ConvertToSQLDate($InvoiceDetails['trandate']);
@@ -360,9 +359,9 @@ function ConvertToSQLDate($DateEntry) {
 		}
 	}
 
-/* Create a customer invoice in webERP. This function will bypass the 
+/* Create a customer credit note in webERP. This function will bypass the
  * normal procedure in webERP for creating a sales order first, and then
- * delivering it.
+ * delivering it. All values should be sent as negatives.
  */
 	function InsertSalesCredit($CreditDetails, $user, $password) {
 		$Errors = array();
@@ -375,92 +374,97 @@ function ConvertToSQLDate($DateEntry) {
 			$CreditDetails[$key] = DB_escape_string($value);
 		}
 		$PartCode=$CreditDetails['partcode'];
+		$Errors=VerifyStockCodeExists($PartCode, sizeof($Errors), $Errors, $db );
 		unset($CreditDetails['partcode']);
 		$SalesArea=$CreditDetails['salesarea'];
 		unset($CreditDetails['salesarea']);
 		$CreditDetails['transno']=GetNextTransactionNo(11, $db);
-		$CreditDetails['type'] = 11;
+		$CreditDetails['type'] = 10;
 		$Errors=VerifyDebtorExists($CreditDetails['debtorno'], sizeof($Errors), $Errors, $db);
 		$Errors=VerifyBranchNoExists($CreditDetails['debtorno'],$CreditDetails['branchcode'], sizeof($Errors), $Errors, $db);
 		$Errors=VerifyTransNO($CreditDetails['transno'], 10, sizeof($Errors), $Errors, $db);
 		$Errors=VerifyTransactionDate($CreditDetails['trandate'], sizeof($Errors), $Errors, $db);
-		$CreditDetails['prd']=GetPeriodFromTransactionDate($CreditDetails['trandate'], sizeof($Errors), $Errors, $db);
 		if (isset($CreditDetails['settled'])){
 			$Errors=VerifySettled($CreditDetails['settled'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['reference'])){
 			$Errors=VerifyReference($CreditDetails['reference'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['tpe'])){
 			$Errors=VerifyTpe($CreditDetails['tpe'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['order_'])){
 			$Errors=VerifyOrderNumber($CreditDetails['order_'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['rate'])){
 			$Errors=VerifyExchangeRate($CreditDetails['rate'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['ovamount'])){
 			$Errors=VerifyOVAmount($CreditDetails['ovamount'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['ovgst'])){
 			$Errors=VerifyOVGst($CreditDetails['ovgst'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['ovfreight'])){
 			$Errors=VerifyOVFreight($CreditDetails['ovfreight'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['ovdiscount'])){
 			$Errors=VerifyOVDiscount($CreditDetails['ovdiscount'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['diffonexch'])){
 			$Errors=VerifyDiffOnExchange($CreditDetails['diffonexch'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['alloc'])){
 			$Errors=VerifyAllocated($CreditDetails['alloc'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['invtext'])){
 			$Errors=VerifyInvoiceText($CreditDetails['invtext'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['shipvia'])){
 			$Errors=VerifyShipVia($CreditDetails['shipvia'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['edisent'])){
 			$Errors=VerifyEdiSent($CreditDetails['edisent'], sizeof($Errors), $Errors);
-		}	
+		}
 		if (isset($CreditDetails['consignment'])){
 			$Errors=VerifyConsignment($CreditDetails['consignment'], sizeof($Errors), $Errors);
-		}	
+		}
 		$FieldNames='';
 		$FieldValues='';
-		$CreditDetails['trandate']=ConvertSQLDate($CreditDetails['trandate']);
+		$CreditDetails['trandate']=ConvertToSQLDate($CreditDetails['trandate']);
+		$CreditDetails['prd']=GetPeriodFromTransactionDate($CreditDetails['trandate'], sizeof($Errors), $Errors, $db);
 		foreach ($CreditDetails as $key => $value) {
 			$FieldNames.=$key.', ';
 			$FieldValues.='"'.$value.'", ';
 		}
 		if (sizeof($Errors)==0) {
+			$result = DB_query('BEGIN',$db);
 			$sql = 'INSERT INTO debtortrans ('.substr($FieldNames,0,-2).') '.
 		  		'VALUES ('.substr($FieldValues,0,-2).') ';
 			$result = DB_Query($sql, $db);
+			$sql = 'UPDATE systypes set typeno='.GetNextTransactionNo(11, $db).' where typeid=10';
+			$result = DB_Query($sql, $db);
 			$SalesGLCode=GetSalesGLCode($SalesArea, $PartCode, $db);
 			$DebtorsGLCode=GetDebtorsGLCode($db);
-			$sql='insert into gltrans Values("null", 11,'.GetNextTransactionNo(11, $db).
+			$sql='insert into gltrans Values("null", 10,'.GetNextTransactionNo(11, $db).
 				',0,"'.$CreditDetails['trandate'].'",'.$CreditDetails['prd'].', '.$DebtorsGLCode.
 				',"'.'Invoice for -'.$CreditDetails['debtorno'].' Total - '.$CreditDetails['ovamount'].
-				'", '.-intval($CreditDetails['ovamount']).', 1,"'.$CreditDetails['jobref'].'")';
+				'", '.$CreditDetails['ovamount'].', 0,"'.$CreditDetails['jobref'].'")';
 			$result = DB_Query($sql, $db);
-			$sql='insert into gltrans Values("null", 11,'.GetNextTransactionNo(11, $db).
+			$sql='insert into gltrans Values("null", 10,'.GetNextTransactionNo(11, $db).
 				',0,"'.$CreditDetails['trandate'].'",'.$CreditDetails['prd'].', '.$SalesGLCode.
 				',"'.'Invoice for -'.$CreditDetails['debtorno'].' Total - '.$CreditDetails['ovamount'].
-				'", '.intval($CreditDetails['ovamount']).', 1,"'.$CreditDetails['jobref'].'")';
+				'", '.-intval($CreditDetails['ovamount']).', 0,"'.$CreditDetails['jobref'].'")';
 			$result = DB_Query($sql, $db);
-			$sql = 'UPDATE systypes set typeno='.GetNextTransactionNo(11, $db).' where typeid=11';
-			$result = DB_Query($sql, $db);
+			$result= DB_query('COMMIT',$db);
 			if (DB_error_no($db) != 0) {
 				$Errors[0] = DatabaseUpdateFailed;
 			} else {
 				$Errors[0]=0;
 			}
+		} else {
+			return $Errors;
 		}
 	}
-	
+
 ?>
