081917681412bfaa39877f392dae9f2a7ed76665
tim_schofield <>
2008-12-04 12:27:12 +0000

    Improve javascript functions nad reduce size

--- javascripts/MiscFunctions.js
+++ javascripts/MiscFunctions.js
@@ -2,6 +2,10 @@ function setTextAlign(control, alignment) {
 	control.style.textAlign=alignment;
 }
 
+function setBGColour(control, colour) {
+	control.style.backgroundColor=colour;
+}
+
 function numberFormat(control, dp) {
 	control.value=parseFloat(control.value).toFixed(dp);
 }
@@ -30,95 +34,58 @@ function assignComboToInput(combo, input) {
 	input.value=combo.value;
 }
 
-function inArray(value, thisArray, msg) {
+function inArray(control, value, thisArray, msg) {
 	for (i=0; i<thisArray.length; i++) {
 		if (( value == thisArray[i].value )) {
+			setBGColour(control, '#ffffff');
 			return true;
 		}
 	}
+	setBGColour(control, '#fddbdb');
 	alert(msg);
 	return false;
 }
 
-var dtCh= "/";
-var minYear=1900;
-var maxYear=2100;
-
-function isInteger(s){
-	var i;
-    for (i = 0; i < s.length; i++){
-        var c = s.charAt(i);
-        if (((c < "0") || (c > "9"))) return false;
-    }
-    return true;
-}
-
-function stripCharsInBag(s, bag){
-	var i;
-    var returnString = "";
-    for (i = 0; i < s.length; i++){
-        var c = s.charAt(i);
-        if (bag.indexOf(c) == -1) returnString += c;
-    }
-    return returnString;
-}
-
-function daysInFebruary (year){
-    return (((year % 4 == 0) && ( (!(year % 100 == 0)) || (year % 400 == 0))) ? 29 : 28 );
-}
-
-function DaysArray(n) {
-	for (var i = 1; i <= n; i++) {
-		this[i] = 31
-		if (i==4 || i==6 || i==9 || i==11) {this[i] = 30}
-		if (i==2) {this[i] = 29}
-   }
-   return this
-}
-
-function isDate(dtStr, dtFormat){
-	var daysInMonth = DaysArray(12)
-	var pos1=dtStr.indexOf(dtCh)
-	var pos2=dtStr.indexOf(dtCh,pos1+1)
-	if (( dtFormat=='m/d/Y' )) {
-		var strMonth=dtStr.substring(0,pos1)
-		var strDay=dtStr.substring(pos1+1,pos2)
+function isDate(control, dateStr, dateFmt) {
+	var matchArray = dateStr.match(/^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/);
+	if (matchArray == null) {
+		setBGColour(control, '#fddbdb');
+		alert("Please enter the date in the format "+dateFmt+". Your current selection reads: " + dateStr);
+		return false;
 	}
-	if (( dtFormat=='d/m/Y' )) {
-		var strMonth=dtStr.substring(pos1+1,pos2)
-		var strDay=dtStr.substring(0,pos1)
+	if (dateFmt=="d/m/Y") {
+		day = matchArray[1];
+		month = matchArray[3];
+	} else {
+		day = matchArray[3];
+		month = matchArray[1];
 	}
-	var strYear=dtStr.substring(pos2+1)
-	strYr=strYear
-	if (strDay.charAt(0)=="0" && strDay.length>1) strDay=strDay.substring(1)
-	if (strMonth.charAt(0)=="0" && strMonth.length>1) strMonth=strMonth.substring(1)
-	for (var i = 1; i <= 3; i++) {
-		if (strYr.charAt(0)=="0" && strYr.length>1) strYr=strYr.substring(1)
+	year = matchArray[5];
+	if (month < 1 || month > 12) {
+		setBGColour(control, '#fddbdb');
+		alert("Month must be between 1 and 12.");
+		return false;
 	}
-	month=parseInt(strMonth)
-	day=parseInt(strDay)
-	year=parseInt(strYr)
-	if (pos1==-1 || pos2==-1){
-		alert("The date format should be : "+dtFormat)
-		return false
+	if (day < 1 || day > 31) {
+		setBGColour(control, '#fddbdb');
+		alert("Day must be between 1 and 31.");
+		return false;
 	}
-	if (strMonth.length<1 || month<1 || month>12){
-		alert("Please enter a valid month")
-		return false
+	if ((month==4 || month==6 || month==9 || month==11) && day==31) {
+		setBGColour(control, '#fddbdb');
+		alert("Month "+month+" doesn`t have 31 days!");
+		return false;
 	}
-	if (strDay.length<1 || day<1 || day>31 || (month==2 && day>daysInFebruary(year)) || day > daysInMonth[month]){
-		alert("Please enter a valid day")
-		return false
+	if (month == 2) {
+		var isleap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+		if (day > 29 || (day==29 && !isleap)) {
+			setBGColour(control, '#fddbdb');
+			alert("February " + year + " doesn`t have " + day + " days!");
+			return false;
+		}
 	}
-	if (strYear.length != 4 || year==0 || year<minYear || year>maxYear){
-		alert("Please enter a valid 4 digit year between "+minYear+" and "+maxYear)
-		return false
-	}
-	if (dtStr.indexOf(dtCh,pos2+1)!=-1 || isInteger(stripCharsInBag(dtStr, dtCh))==false){
-		alert("Please enter a valid date")
-		return false
-	}
-	return true
+	setBGColour(control, '#ffffff');
+	return true;
 }
 
 function eitherOr(one, two) {
