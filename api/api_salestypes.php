cf8955d885d61e71e64c70ad5738b4947db49f13
tim_schofield <>
2009-02-01 11:59:50 +0000

    Add InsertSalesType() function and correct return values in other functions

--- api/api_salestypes.php
+++ api/api_salestypes.php
@@ -1,7 +1,7 @@
 <?php
 
 /* This function returns a list of the sales type abbreviations
- * currently setup on webERP 
+ * currently setup on webERP
  */
 
 	function GetSalesTypeList($user, $password) {
@@ -18,14 +18,16 @@
 			$SalesTypeList[$i]=$myrow[0];
 			$i++;
 		}
-		return $SalesTypeList;
+		$Errors[0]=0;
+		$Errors[1]=$SalesTypeList;
+		return $Errors;
 	}
-	
+
 /* This function takes as a parameter a sales type abbreviation
- * and returns an array containing the details of the selected 
+ * and returns an array containing the details of the selected
  * sales type.
  */
-	
+
 	function GetSalesTypeDetails($salestype, $user, $password) {
 		$Errors = array();
 		$db = db($user, $password);
@@ -33,9 +35,47 @@
 			$Errors[0]=NoAuthorisation;
 			return $Errors;
 		}
-		$sql = 'SELECT * FROM salestypes WHERE typeabbrev="'.$salestype.'"';
-		$result = DB_query($sql, $db);
-		return DB_fetch_array($result);
+		$Errors = VerifySalesType($salestype, sizeof($Errors), $Errors, $db);
+		if (sizeof($Errors)==0) {
+			$sql = 'SELECT * FROM salestypes WHERE typeabbrev="'.$salestype.'"';
+			$result = DB_query($sql, $db);
+			$Errors[0]=0;
+			$Errors[1]=DB_fetch_array($result);
+			return $Errors;
+		} else {
+			return $Errors;
+		}
+	}
+
+/* This function takes as a parameter an array of sales type details
+ * to be inserted into webERP.
+ */
+
+	function InsertSalesType($SalesTypeDetails, $user, $password) {
+		$Errors = array();
+		$db = db($user, $password);
+		if (gettype($db)=='integer') {
+			$Errors[0]=NoAuthorisation;
+			return $Errors;
+		}
+
+		$FieldNames='';
+		$FieldValues='';
+		foreach ($SalesTypeDetails as $key => $value) {
+			$FieldNames.=$key.', ';
+			$FieldValues.='"'.$value.'", ';
+		}
+		$sql = 'INSERT INTO salestypes ('.substr($FieldNames,0,-2).') '.
+		  'VALUES ('.substr($FieldValues,0,-2).') ';
+		if (sizeof($Errors)==0) {
+			$result = DB_Query($sql, $db);
+			if (DB_error_no($db) != 0) {
+				$Errors[0] = DatabaseUpdateFailed;
+			} else {
+				$Errors[0]=0;
+			}
+		}
+		return $Errors;
 	}
 
 ?>
