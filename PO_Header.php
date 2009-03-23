b23417f787229a0ecde21fca5230f99e584acb3d
tim_schofield <>
2009-03-23 20:12:53 +0000

    Show location address when the lookup address button is pushed

--- PO_Header.php
+++ PO_Header.php
@@ -1,6 +1,6 @@
 <?php
 
-/* $Revision: 1.21 $ */
+/* $Revision: 1.22 $ */
 
 /*
  *      PO_Header.php
@@ -382,7 +382,7 @@ if ($_SESSION['RequireSupplierSelection'] == 1 OR !isset($_SESSION['PO']->Suppli
 
 	/* Set up form for entry of order header stuff */
 
-	if ((!isset($_POST['StkLocation']) or $_POST['StkLocation']) AND (isset($_SESSION['PO']->Location) AND $_SESSION['PO']->Location != '')) {
+	if (!isset($_POST['LookupDeliveryAddress']) and (!isset($_POST['StkLocation']) or $_POST['StkLocation']) AND (isset($_SESSION['PO']->Location) AND $_SESSION['PO']->Location != '')) {
 	    /* The session variables are set but the form variables have
 	     * been lost --
 	     * need to restore the form variables from the session */
