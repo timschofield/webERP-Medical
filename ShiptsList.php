196d52f1fe6a1e8dc0bab5a2f93b04c436673224
tim_schofield <>
2009-06-16 11:45:56 +0000

    Change to lower case html and remove <center> tags

--- ShiptsList.php
+++ ShiptsList.php
@@ -1,6 +1,6 @@
 <?php
 
-/* $Revision: 1.8 $ */
+/* $Revision: 1.9 $ */
 
 $PageSecurity = 2;
 include ('includes/session.inc');
@@ -9,7 +9,7 @@ include('includes/header.inc');
 
 
 if (!isset($_GET['SupplierID']) OR !isset($_GET['SupplierName'])){
-	echo '<P>';
+	echo '<p>';
 	prnMsg( _('This page must be given the supplier code to look for shipments for'), 'error');
 	include('includes/footer.inc');
 	exit;
@@ -30,22 +30,22 @@ if (DB_num_rows($ShiptsResult)==0){
 }
 /*show a table of the shipments returned by the SQL */
 
-echo '<CENTER><FONT SIZE=4 COLOR=BLUE>'. _('Open Shipments for').' ' . $_GET['SupplierName'] . '</FONT><BR>
-	<TABLE CELLPADDING=2 COLSPAN=2>';
-$TableHeader = '<TR>
-		<TH>'. _('Reference'). '</TH>
-		<TH>'. _('Vessel'). '</TH>
-		<TH>'. _('ETA'). '</TH></TR>';
+echo '<div class="centre"><font size=4 color=BLUE>'. _('Open Shipments for').' ' . $_GET['SupplierName'] . '</font><br>
+	</div><table cellpadding=2 colspan=2>';
+$TableHeader = '<tr>
+		<th>'. _('Reference'). '</th>
+		<th>'. _('Vessel'). '</th>
+		<th>'. _('ETA'). '</th></tr>';
 
 $j = 1;
 $k = 0; //row colour counter
 
 while ($myrow=DB_fetch_array($ShiptsResult)) {
        if ($k==1){
-              echo '<TR class="OddTableRows">';
+              echo '<tr class="OddTableRows">';
               $k=0;
        } else {
-              echo '<TR class="EvenTableRows">';
+              echo '<tr class="EvenTableRows">';
               $k=1;
        }
 
@@ -65,7 +65,7 @@ while ($myrow=DB_fetch_array($ShiptsResult)) {
 }
 //end of while loop
 
-echo '</TABLE></CENTER>';
+echo '</table>';
 
 include('includes/footer.inc');
 
