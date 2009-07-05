583be0c11ca08fa79167395a4ce9ab1c51084d9f
tim_schofield <>
2009-07-05 07:20:34 +0000

    Function set_magic_quotes_runtime() is deprecated

--- includes/fpdf.php
+++ includes/fpdf.php
@@ -1162,7 +1162,7 @@ function _putfonts()
 		$this->_out('endobj');
 	}
 	$mqr=get_magic_quotes_runtime();
-	set_magic_quotes_runtime(0);
+	@set_magic_quotes_runtime(0);
 	foreach($this->FontFiles as $file=>$info)
 	{
 		//Font file embedding
@@ -1200,7 +1200,7 @@ function _putfonts()
 		$this->_putstream($font);
 		$this->_out('endobj');
 	}
-	set_magic_quotes_runtime($mqr);
+	@set_magic_quotes_runtime($mqr);
 	foreach($this->fonts as $k=>$font)
 	{
 		//Font objects
