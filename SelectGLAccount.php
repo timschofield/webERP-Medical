66ecd5010739c1d1e2382e792e543aef06876da8
emdeex <>
2009-08-06 22:07:48 +0000

    small html formatting fixes

--- SelectGLAccount.php
+++ SelectGLAccount.php
@@ -1,5 +1,5 @@
 <?php
-/* $Revision: 1.11 $ */
+/* $Revision: 1.12 $ */
 
 $PageSecurity = 8;
 
@@ -22,9 +22,9 @@ If (isset($_POST['Select'])) {
 	$AccountID = $_POST['Select'];
 	$_POST['Select'] = NULL;
 
-	echo '<br><a href="' . $rootpath . '/GLAccounts.php?' . SID . '&SelectedAccount=' . $AccountID . '">' . _('Edit Account') . '</a>';
+	echo '<br><div class="centre"><a href="' . $rootpath . '/GLAccounts.php?' . SID . '&SelectedAccount=' . $AccountID . '">' . _('Edit Account') . '</a>';
 	echo '<br><a href="' . $rootpath . '/GLAccountInquiry.php?' . SID . '&Account=' . $AccountID . '">' . _('Account Inquiry') . '</a>';
-	echo '<div class="centre><a href="' . $_SERVER['PHP_SELF'] . '?' . SID .  '">' . _('New Search') . '</a></div>';
+	echo '<br><a href="' . $_SERVER['PHP_SELF'] . '?' . SID .  '">' . _('New Search') . '</a></div>';
 
 } elseif (isset($_POST['Search'])){
 
