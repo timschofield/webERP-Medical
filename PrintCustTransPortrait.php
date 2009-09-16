11b836aae91758f8b0a51e1e53bf7cba2ce4fe0d
emdeex <>
2009-09-16 03:27:06 +0000

    I've put this SQL back as it is now required.  I will raise a trac to provide a user hint when a default bank account is not set.

--- PrintCustTransPortrait.php
+++ PrintCustTransPortrait.php
@@ -142,7 +142,8 @@ If (isset($PrintPDF)
 			AND debtortrans.debtorno=custbranch.debtorno
 			AND debtortrans.branchcode=custbranch.branchcode
 			AND custbranch.salesman=salesman.salesmancode
-			AND salesorders.fromstkloc=locations.loccode';
+			AND salesorders.fromstkloc=locations.loccode
+			AND bankaccounts.invoice = 1';
 
 		if (isset($_POST['PrintEDI']) and $_POST['PrintEDI']=='No'){
 			$sql = $sql . ' AND debtorsmaster.ediinvoices=0';
