3b388478e9f690df7989d7d9afb79bcefe44bc91
tim_schofield <>
2009-12-14 14:04:35 +0000

    Merge utf-8 branch into the main line

--- PDFDeliveryDifferences.php
+++ PDFDeliveryDifferences.php
@@ -1,6 +1,8 @@
 <?php
 
-/* $Revision: 1.14 $ */
+/*$Id$*/
+
+/* $Revision: 1.15 $ */
 
 $PageSecurity = 3;
 include ('includes/session.inc');
@@ -154,10 +156,10 @@ if (DB_error_no($db)!=0){
 	}
 	include ('includes/footer.inc');
 	exit;
-} elseif (DB_num_rows($Result)==0){
+} elseif (DB_num_rows($Result) == 0){
 	$title = _('Delivery Differences Log Report Error');
   	include('includes/header.inc');
-	prnMsg( _('There were no variances between deliveries and orders found in the database within the period from') . ' ' . $_POST['FromDate'] . ' ' . _('to') . ' ' . $_POST['ToDate'] . '. ' . _('Please try again selecting a different date range'),'info');
+	prnMsg( _('There were no variances between deliveries and orders found in the database within the period from') . ' ' . $_POST['FromDate'] . ' ' . _('to') . ' ' . $_POST['ToDate'] . '. ' . _('Please try again selecting a different date range'), 'info');
 	if ($debug==1) {
 		prnMsg( _('The SQL that returned no rows was') . '<br>' . $sql,'error');
 	}
@@ -169,12 +171,10 @@ include('includes/PDFStarter.php');
 
 /*PDFStarter.php has all the variables for page size and width set up depending on the users default preferences for paper size */
 
-$pdf->addinfo('Title',_('Variances Between Deliveries and Orders'));
-$pdf->addinfo('Subject',_('Variances Between Deliveries and Orders from') . ' ' . $_POST['FromDate'] . ' ' . _('to') . ' ' . $_POST['ToDate']);
-
+$pdf->addInfo('Title',_('Variances Between Deliveries and Orders'));
+$pdf->addInfo('Subject',_('Variances Between Deliveries and Orders from') . ' ' . $_POST['FromDate'] . ' ' . _('to') . ' ' . $_POST['ToDate']);
 $line_height=12;
 $PageNumber = 1;
-
 $TotalDiffs = 0;
 
 include ('includes/PDFDeliveryDifferencesPageHeader.inc');
@@ -253,7 +253,7 @@ $LeftOvers = $pdf->addTextWrap($Left_Margin,$YPos,200,$FontSize,_('Total number 
 $YPos-=$line_height;
 $LeftOvers = $pdf->addTextWrap($Left_Margin,$YPos,200,$FontSize,_('DIFOT') . ' ' . number_format((1-($TotalDiffs/$myrow[0])) * 100,2) . '%', 'left');
 
-
+/* UldisN
 $pdfcode = $pdf->output();
 $len = strlen($pdfcode);
 header('Content-type: application/pdf');
@@ -264,21 +264,24 @@ header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Pragma: public');
 
 $pdf->stream();
+*/
+$ReportFileName = $_SESSION['DatabaseName'] . '_DeliveryDifferences_' . date('Y-m-d').'.pdf';
+$pdf->OutputD($ReportFileName);//UldisN
 
 if ($_POST['Email']=='Yes'){
-	if (file_exists($_SESSION['reports_dir'] . '/DeliveryDifferences.pdf')){
-		unlink($_SESSION['reports_dir'] . '/DeliveryDifferences.pdf');
+	if (file_exists($_SESSION['reports_dir'] . '/'.$ReportFileName)){
+		unlink($_SESSION['reports_dir'] . '/'.$ReportFileName);
 	}
-    	$fp = fopen( $_SESSION['reports_dir'] . '/DeliveryDifferences.pdf','wb');
+    	$fp = fopen( $_SESSION['reports_dir'] . '/'.$ReportFileName,'wb');
 	fwrite ($fp, $pdfcode);
 	fclose ($fp);
 
 	include('includes/htmlMimeMail.php');
 
 	$mail = new htmlMimeMail();
-	$attachment = $mail->getFile($_SESSION['reports_dir'] . '/DeliveryDifferences.pdf');
+	$attachment = $mail->getFile($_SESSION['reports_dir'] . '/'.$ReportFileName);
 	$mail->setText(_('Please find herewith delivery differences report from') . ' ' . $_POST['FromDate'] .  ' '. _('to') . ' ' . $_POST['ToDate']);
-	$mail->addAttachment($attachment, 'DeliveryDifferences.pdf', 'application/pdf');
+	$mail->addAttachment($attachment, $ReportFileName, 'application/pdf');
 	$mail->setFrom($_SESSION['CompanyRecord']['coyname'] . '<' . $_SESSION['CompanyRecord']['email'] .'>');
 
 	/* $DelDiffsRecipients defined in config.php */
