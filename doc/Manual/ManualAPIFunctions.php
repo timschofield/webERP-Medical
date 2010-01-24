129d66d743dc9780aa9d6d3011e6a44a82d28432
daintree <>
2010-01-24 02:04:47 +0000

    fix path for hiawatha

--- doc/Manual/ManualAPIFunctions.php
+++ doc/Manual/ManualAPIFunctions.php
@@ -13,7 +13,7 @@ $title = 'API documentation';
 echo '<html xmlns="http://www.w3.org/1999/xhtml"><head><title>' . $title . '</title>';
 echo '<link REL="shortcut icon" HREF="'. $rootpath.'/favicon.ico">';
 echo '<link REL="icon" HREF="' . $rootpath.'/favicon.ico">';
-echo '<meta http-equiv="Content-Type" content="text/html; charset=' . _('ISO-8859-1') . '">';
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
 echo '<link href="'.$rootpath. '/../../css/'. $_SESSION['Theme'] .'/default.css" REL="stylesheet" TYPE="text/css">';
 echo '</head>';
 
@@ -25,7 +25,11 @@ $result=DB_query($sql, $db);
 $myrow=DB_fetch_array($result);
 $weberppassword = $myrow[0];
 
-$ServerURL = "http://". $_SERVER['HTTP_HOST'].$rootpath."/../../api/api_xml-rpc.php";
+$ServerString = $_SERVER['HTTP_HOST'].$rootpath;
+$FirstBitOfURL = substr($ServerString,0,strpos($ServerString,'/doc/Manual'));
+
+
+$ServerURL = "http://".  $FirstBitOfURL ."/api/api_xml-rpc.php";
 $DebugLevel = 0; //Set to 0,1, or 2 with 2 being the highest level of debug info
 
 $msg = new xmlrpcmsg("system.listMethods", array());
