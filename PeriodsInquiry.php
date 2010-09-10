3f1b7159634e39265d12973dbe9c6afda49590c1
Tim Schofield <tim@weberpafrica.com>
2010-09-10 11:11:14 +0100

    Layout improvements

--- PeriodsInquiry.php
+++ PeriodsInquiry.php
@@ -19,39 +19,61 @@ $SQL = "SELECT periodno ,
 $ErrMsg =  _('No periods were returned by the SQL because');
 $PeriodsResult = DB_query($SQL,$db,$ErrMsg);
 
+	echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/transactions.png" title="' . $title . '" alt="">' . ' '
+		. $title . '</p>';
 
 /*show a table of the orders returned by the SQL */
 
-echo '<table cellpadding=2 colspan=2>';
+$NumberOfPeriods = DB_num_rows($PeriodsResult);
+$PeriodsInTable = round($NumberOfPeriods/3,0);
 
 $TableHeader = '<tr><th>' . _('Period Number') . '</th>
 			<th>' . _('Date of Last Day') . '</th>
 		</tr>';
-
-echo $TableHeader;
-
-$j = 1;
-$k=0; //row colour counter
-while ($myrow=DB_fetch_array($PeriodsResult)) {
-       if ($k==1){
-              echo '<tr class="EvenTableRows">';
-              $k=0;
-       } else {
-              echo '<tr class="OddTableRows">';
-              $k++;
-       }
-
-       $FormatedLastDate = ConvertSQLDate($myrow['lastdate_in_period']);
-       printf("<td><font size=2>%s</td>
-		<td>%s</td>
-		</tr>",
-		$myrow['periodno'],
-		$FormatedLastDate);
-
+echo '<table><tr>';
+for ($i=0;$i<2;$i++) {
+	echo '<td>';
+	echo '<table cellpadding=2 colspan=2 class=selection>';
+	echo $TableHeader;
+	$k=0;
+	for ($j=0; $j<$PeriodsInTable;$j++) {
+		$myrow=DB_fetch_array($PeriodsResult);
+		if ($k==1){
+			echo '<tr class="EvenTableRows">';
+			$k=0;
+		} else {
+			echo '<tr class="OddTableRows">';
+			$k++;
+		}
+		$FormatedLastDate = ConvertSQLDate($myrow['lastdate_in_period']);
+		echo "<td>".$myrow['periodno']."</td>
+			<td>".$FormatedLastDate."</td>
+			</tr>";
+	}
+	echo '</table>';
+	echo '</td>';
 }
-//end of while loop
-
+echo '<td>';
+echo '<table cellpadding=2 colspan=2 class=selection>';
+echo $TableHeader;
+$k = 0; //row colour counter
+while ($myrow=DB_fetch_array($PeriodsResult)) {
+	if ($k==1){
+		echo '<tr class="EvenTableRows">';
+		$k=0;
+	} else {
+		echo '<tr class="OddTableRows">';
+		$k++;
+	}
+	$FormatedLastDate = ConvertSQLDate($myrow['lastdate_in_period']);
+	echo "<td>".$myrow['periodno']."</td>
+		<td>".$FormatedLastDate."</td>
+		</tr>";
+}
 echo '</table>';
+echo '</td>';
+echo '</tr></table>';
+//end of while loop
 
 include('includes/footer.inc');
 ?>
