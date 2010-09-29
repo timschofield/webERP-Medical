a2c489e0d73f04172d3ea6818070f03209b3284c
Tim Schofield <tim@weberpafrica.com>
2010-09-29 22:21:50 +0100

    SQL quoting corrections and layout changes and improvements

--- WorkOrderStatus.php
+++ WorkOrderStatus.php
@@ -25,7 +25,7 @@ include('includes/header.inc');
 			INNER JOIN stockmaster
 			ON woitems.stockid=stockmaster.stockid
 			WHERE woitems.stockid='" . $_REQUEST['StockID'] . "'
-			AND woitems.wo =" . $_REQUEST['WO'],
+			AND woitems.wo ='" . $_REQUEST['WO'] . "'",
 			$db,
 			$ErrMsg);
 
@@ -35,11 +35,14 @@ include('includes/header.inc');
 		exit;
 	}
 	$WORow = DB_fetch_array($WOResult);
-	
+
 	echo '<a href="'. $rootpath . '/SelectWorkOrder.php?' . SID . '">' . _('Back to Work Orders'). '</a><br>';
     echo '<a href="'. $rootpath . '/WorkOrderCosting.php?' . SID . '&WO=' .  $_REQUEST['WO'] . '">' . _('Back to Costing'). '</a><br>';
-    
-	echo '<table cellpadding=2 border=1>
+
+	echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/group_add.png" title="' .
+		_('Search') . '" alt="">' . ' ' . $title.'</p';
+
+	echo '<table cellpadding=2 class=selection>
 		<tr><td class="label">' . _('Issue to work order') . ':</td><td>' . $_REQUEST['WO'] .'</td><td class="label">' . _('Item') . ':</td><td>' . $_REQUEST['StockID'] . ' - ' . $WORow['description'] . '</td></tr>
 	 	<tr><td class="label">' . _('Manufactured at') . ':</td><td>' . $WORow['locationname'] . '</td><td class="label">' . _('Required By') . ':</td><td>' . ConvertSQLDate($WORow['requiredby']) . '</td></tr>
 	 	<tr><td class="label">' . _('Quantity Ordered') . ':</td><td class=number>' . number_format($WORow['qtyreqd'],$WORow['decimalplaces']) . '</td><td colspan=2>' . $WORow['units'] . '</td></tr>
@@ -56,11 +59,11 @@ include('includes/header.inc');
 				$db);
 		$LocRow = DB_fetch_array($LocResult);
 		echo $LocRow['locationname'];
-		echo '<tr><td colspan=4><hr></td></tr>';
-		echo '</td></tr></table>';
+		echo '<tr><td colspan=4></td></tr>';
+		echo '</td></tr></table><br />';
 
 		//set up options for selection of the item to be issued to the WO
-		echo '<table border=1><tr><th colspan=5>' . _('Material Requirements For this Work Order') . '</th></tr>';
+		echo '<table class=selection><tr><th colspan=5><font size=2 color=navy>' . _('Material Requirements For this Work Order') . '</font></th></tr>';
 		echo '<tr><th colspan=2>' . _('Item') . '</th>
 			<th>' . _('Qty Required') . '</th>
 			<th>' . _('Qty Issued') . '</th></tr>';
@@ -72,7 +75,7 @@ include('includes/header.inc');
 						qtypu
 					FROM worequirements INNER JOIN stockmaster
 					ON worequirements.stockid=stockmaster.stockid
-					WHERE wo=" . $_REQUEST['WO'],
+					WHERE wo='" . $_REQUEST['WO'] . "'",
 					$db);
 
 		while ($RequirementsRow = DB_fetch_array($RequirmentsResult)){
@@ -88,7 +91,7 @@ include('includes/header.inc');
 							AND reference='" . $_REQUEST['WO'] . "'",
 						$db);
 			$IssuedAlreadyRow = DB_fetch_row($IssuedAlreadyResult);
-		
+
 			echo '<td align="right">' . number_format($WORow['qtyreqd']*$RequirementsRow['qtypu'],$RequirementsRow['decimalplaces']) . '</td>
 				<td align="right">' . number_format($IssuedAlreadyRow[0],$RequirementsRow['decimalplaces']) . '</td></tr>';
 		}
