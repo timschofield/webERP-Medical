bdd77ac774479d83af68d55df0de1c53d7b0a149
Tim Schofield <tim@weberpafrica.com>
2010-09-30 12:13:05 +0100

    Add form verification to prevent form spoofing

--- Locations.php
+++ Locations.php
@@ -407,6 +407,7 @@ echo "<p>";
 if (!isset($_GET['delete'])) {
 
 	echo "<form method='post' action='" . $_SERVER['PHP_SELF'] . '?' . SID . "'>";
+	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 	if (isset($SelectedLocation)) {
 		//editing an existing Location
