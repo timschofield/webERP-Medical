4602088e8124a9b932340ee0271f6e38e40a503a
Tim Schofield <tim@weberpafrica.com>
2010-09-30 14:00:20 +0100

    Add form verification to prevent form spoofing

--- SalesGLPostings.php
+++ SalesGLPostings.php
@@ -271,6 +271,7 @@ if (isset($SelectedSalesPostingID)) {
 if (!isset($_GET['delete'])) {
 
 	echo "<form method='post' action=" . $_SERVER['PHP_SELF'] . '?' . SID . '>';
+	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 	if (isset($SelectedSalesPostingID)) {
 		//editing an existing sales posting record
