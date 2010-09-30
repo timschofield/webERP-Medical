1b0bbaaaef1505ab82f4cdb68a293c945b6ce585
Tim Schofield <tim@weberpafrica.com>
2010-09-30 14:28:14 +0100

    Add form verification to prevent form spoofing

--- TaxCategories.php
+++ TaxCategories.php
@@ -199,6 +199,7 @@ echo '<p>';
 if (! isset($_GET['delete'])) {
 
 	echo "<form method='post' action=" . $_SERVER['PHP_SELF'] . '?' . SID . '>';
+	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 	if (isset($SelectedTaxCategory)) {
 		//editing an existing section
