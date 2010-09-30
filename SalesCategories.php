5e0f898f5131ae7485bd24458c7e6ab7babcc860
Tim Schofield <tim@weberpafrica.com>
2010-09-30 13:59:43 +0100

    Add form verification to prevent form spoofing

--- SalesCategories.php
+++ SalesCategories.php
@@ -289,6 +289,7 @@ echo '</p>';
 // Show New or Edit Category
 
 echo '<p><form ENCtype="MULTIPART/FORM-DATA" method="POST" action="' . $_SERVER['PHP_SELF'] . '?' . SID . '">';
+echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 // This array will contain the stockids in use for this category
 if (isset($SelectedCategory)) {
@@ -372,6 +373,7 @@ $result = DB_query($sql,$db);
 if($result && DB_num_rows($result)) {
 	// continue id stock id in the stockid array
 	echo '<p><form ENCtype="MULTIPART/FORM-DATA" method="POST" action="' . $_SERVER['PHP_SELF'] . '?' . SID . '">';
+	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 	if( isset($SelectedCategory) ) { // If we selected a category we need to keep it selected
 		echo '<input type=hidden name="SelectedCategory" VALUE="' . $SelectedCategory . '">';
 	}
