d375ec604d6ade24b3888af28f1e61dfb8a52573
Tim Schofield <tim@weberpafrica.com>
2010-09-30 14:41:26 +0100

    Add form verification to prevent form spoofing

--- Z_MakeNewCompany.php
+++ Z_MakeNewCompany.php
@@ -26,26 +26,27 @@ if (! is_writeable('./companies/')){
 		exit;
 }
 
- 
+
 if (isset($_POST['submit']) AND isset($_POST['NewCompany'])) {
-	
-	if(strlen($_POST['NewCompany'])>32 
+
+	if(strlen($_POST['NewCompany'])>32
 		OR ContainsIllegalCharacters($_POST['NewCompany'])){
 		prnMsg(_('Company abbreviations must not contain spaces, \& or " or \''),'error');
 	} else {
-				
+
 		$_POST['NewCompany'] = strtolower($_POST['NewCompany']);
 		echo '<div class="centre">';
 		echo '<form method="post" action=' . $_SERVER['PHP_SELF'] . '?' . SID . '>';
+		echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 		/* check for directory existence */
-		if (!file_exists('./companies/' . $_POST['NewCompany']) 
+		if (!file_exists('./companies/' . $_POST['NewCompany'])
 				AND (isset($_FILES['LogoFile']) AND $_FILES['LogoFile']['name'] !='')) {
-			
+
 			$result    = $_FILES['LogoFile']['error'];
 			$UploadTheLogo = 'Yes'; //Assume all is well to start off with
 			$filename = './companies/' . $_POST['NewCompany'] . '/logo.jpg';
-						
-			//But check for the worst 
+
+			//But check for the worst
 			if (strtoupper(substr(trim($_FILES['LogoFile']['name']),strlen($_FILES['LogoFile']['name'])-3))!='JPG'){
 				prnMsg(_('Only jpg files are supported - a file extension of .jpg is expected'),'warn');
 				$UploadTheLogo ='No';
@@ -63,19 +64,19 @@ if (isset($_POST['submit']) AND isset($_POST['NewCompany'])) {
 					$UploadTheLogo ='No';
 				}
 			}
-			
+
 			if ($_POST['CreateDB']==TRUE){
 				/* Need to read in the sql script and process the queries to initate a new DB */
-			
+
 				$result = DB_query('CREATE DATABASE ' . $_POST['NewCompany'],$db);
-			
+
 				if ($dbType=='postgres'){
-					
+
 					$PgConnStr = 'dbname=' . $_POST['NewCompany'];
 					if ( isset($host) && ($host != "")) {
 						$PgConnStr = 'host=' . $host . ' ' . $PgConnStr;
 					}
-	
+
 					if (isset( $dbuser ) && ($dbuser != "")) {
 						// if we have a user we need to use password if supplied
 						$PgConnStr .= " user=".$dbuser;
@@ -85,7 +86,7 @@ if (isset($_POST['submit']) AND isset($_POST['NewCompany'])) {
 					}
 					$db = pg_connect( $PgConnStr );
 					$SQLScriptFile = file('./sql/pg/weberp-new.psql');
-				
+
 				} elseif ($dbType =='mysql') { //its a mysql db < 4.1
 					mysql_select_db($_POST['NewCompany'],$db);
 					$SQLScriptFile = file('./sql/mysql/weberp-new.sql');
@@ -98,18 +99,18 @@ if (isset($_POST['submit']) AND isset($_POST['NewCompany'])) {
 				$ErrMsg = _('The script to create the new company database failed because');
 				$SQL ='';
 				$InAFunction = false;
-				
+
 				for ($i=0; $i<=$ScriptFileEntries; $i++) {
-					
+
 					$SQLScriptFile[$i] = trim($SQLScriptFile[$i]);
-						
-					if (substr($SQLScriptFile[$i], 0, 2) != '--' 
-						AND substr($SQLScriptFile[$i], 0, 3) != 'USE' 
-						AND strstr($SQLScriptFile[$i],'/*')==FALSE 
+
+					if (substr($SQLScriptFile[$i], 0, 2) != '--'
+						AND substr($SQLScriptFile[$i], 0, 3) != 'USE'
+						AND strstr($SQLScriptFile[$i],'/*')==FALSE
 						AND strlen($SQLScriptFile[$i])>1){
-							
+
 						$SQL .= ' ' . $SQLScriptFile[$i];
-	
+
 						//check if this line kicks off a function definition - pg chokes otherwise
 						if (substr($SQLScriptFile[$i],0,15) == 'CREATE FUNCTION'){
 							$InAFunction = true;
@@ -123,11 +124,11 @@ if (isset($_POST['submit']) AND isset($_POST['NewCompany'])) {
 							$result = DB_query($SQL, $db, $ErrMsg);
 							$SQL='';
 						}
-						
+
 					} //end if its a valid sql line not a comment
 				} //end of for loop around the lines of the sql script
 			} //end if CreateDB was checked
-			
+
 			prnMsg (_('Attempting to create the new company directories') . '.....<br>', 'info');
 			$Result = mkdir('./companies/' . $_POST['NewCompany']);
 			$Result = mkdir('./companies/' . $_POST['NewCompany'] . '/part_pics');
@@ -137,13 +138,13 @@ if (isset($_POST['submit']) AND isset($_POST['NewCompany'])) {
 			$Result = mkdir('./companies/' . $_POST['NewCompany'] . '/EDI_Pending');
 			$Result = mkdir('./companies/' . $_POST['NewCompany'] . '/reportwriter');
 			$Result = mkdir('./companies/' . $_POST['NewCompany'] . '/pdf_append');
-				
+
 			/*OK Now upload the logo */
 			if ($UploadTheLogo=='Yes'){
 				$result  =  move_uploaded_file($_FILES['LogoFile']['tmp_name'], $filename);
 				$message = ($result)?_('File url') ."<a href='". $filename ."'>" .  $filename . '</a>' : _('Something is wrong with uploading a file');
 			}
-			
+
 		} else {
 			prnMsg(_('This company cannot be added because either it already exists or no logo is being uploaded!'),'error');
 			if (isset($_FILES['LogoFile'])){
@@ -156,21 +157,21 @@ if (isset($_POST['submit']) AND isset($_POST['NewCompany'])) {
 			} else  {
 				prnMsg('_FILES[LogoFile][name] ' ._('is blank'),'info');
 			}
-			
+
   			echo '</form>';
 	  		echo '</div>';
 			include('includes/footer.inc');
 			exit;
 		}
-	
+
 		$_SESSION['DatabaseName'] = $_POST['NewCompany'];
-		
+
 		unset ($_SESSION['CustomerID']);
 		unset ($_SESSION['SupplierID']);
 		unset ($_SESSION['StockID']);
 		unset ($_SESSION['Items']);
 		unset ($_SESSION['CreditItems']);
-		
+
 		$SQL ="UPDATE config SET confvalue='companies/" . $_POST['NewCompany'] . "/EDI__Sent' WHERE confname='EDI_MsgSent'";
 		$result = DB_query($SQL,$db);
 		$SQL ="UPDATE config SET confvalue='companies/" . $_POST['NewCompany'] . "/EDI_Incoming_Orders' WHERE confname='EDI_Incoming_Orders'";
@@ -181,16 +182,16 @@ if (isset($_POST['submit']) AND isset($_POST['NewCompany'])) {
 		$result = DB_query($SQL,$db);
 		$SQL ="UPDATE config SET confvalue='companies/" . $_POST['NewCompany'] . "/EDI_Pending' WHERE confname='EDI_MsgPending'";
 		$result = DB_query($SQL,$db);
-		
+
 		$ForceConfigReload=true;
 		include('includes/GetConfig.php');
-		
+
 		prnMsg (_('The new company database has been created for' . ' ' . $_POST['NewCompany'] . '. ' . _('The company details and parameters should now be set up for the new company. NB: Only a single user "demo" is defined with the password "weberp" in the new company database. A new system administrator user should be defined for the new company and this account deleted immediately.')), 'info');
-		
+
 		echo '<p><a href="' . $rootpath . '/CompanyPreferences.php?' . SID .'">' . _('Set Up New Company Details') . '</a>';
 		echo '<p><a href="' . $rootpath . '/SystemParameters.php?' . SID .'">' . _('Set Up Configuration Details') . '</a>';
 		echo '<p><a href="' . $rootpath . '/WWW_Users.php?' . SID .'">' . _('Set Up User Accounts') . '</a>';
-		
+
 		echo '</form>';
 		echo '</div>';
 		include('includes/footer.inc');
@@ -206,6 +207,7 @@ prnMsg (_('This utility will create a new company') . '<br><br>' .
 		_('If the company name already exists then you cannot recreate it'), 'info', _('PLEASE NOTE'));
 echo '<br>';
 echo '<form method="post" action=' . $_SERVER['PHP_SELF'] . '?' . SID . ' enctype="multipart/form-data">';
+echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 echo '<table><tr>';
 echo '<td>' . _('Enter up to 32 character lower case character abbreviation for the company') . '</td>
