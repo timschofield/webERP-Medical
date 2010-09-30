84737760c412c0629a50cd665aad23544c5ba71d
Tim Schofield <tim@weberpafrica.com>
2010-09-30 11:56:41 +0100

    Add form verification to prevent form spoofing

--- CustWhereAlloc.php
+++ CustWhereAlloc.php
@@ -8,6 +8,7 @@ $title = _('Customer How Paid Inquiry');
 include('includes/header.inc');
 
 echo "<form action='" . $_SERVER['PHP_SELF'] . "' method=post>";
+echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/money_add.png" title="' .
 	_('Customer Where Allocated'). '" alt="">' . $title . '</p>';
