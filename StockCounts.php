5c617e3bdd2ed6e8c8f8057d1fe9ed9b7f09b9ac
Tim Schofield <tim@weberpafrica.com>
2010-09-30 14:12:31 +0100

    Add form verification to prevent form spoofing

--- StockCounts.php
+++ StockCounts.php
@@ -10,6 +10,7 @@ $title = _('Stock Check Sheets Entry');
 include('includes/header.inc');
 
 echo "<form action='" . $_SERVER['PHP_SELF'] . "' method=post>";
+echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/inventory.png" title="' .
 	_('Inventory Adjustment') . '" alt="">' . ' ' . $title . '</p>';
