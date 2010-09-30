004df7266e7a951dc83a88f5a765122e3b7017de
Tim Schofield <tim@weberpafrica.com>
2010-09-30 14:02:51 +0100

    Add form verification to prevent form spoofing

--- SelectCompletedOrder.php
+++ SelectCompletedOrder.php
@@ -14,6 +14,7 @@ echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/m
 	'" alt="">' . ' ' . _('Search Sales Orders') . '</p>';
 
 echo "<form action='" . $_SERVER['PHP_SELF'] . '?' . SID ."' method=post>";
+echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 if (isset($_POST['completed'])) {
 	$completed='=1';
