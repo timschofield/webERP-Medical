6259e68701acfead9b96c45c4d06a7823bfe4898
Tim Schofield <tim@weberpafrica.com>
2010-09-30 11:31:24 +0100

    Add form verification to prevent form spoofing

--- AgedSuppliers.php
+++ AgedSuppliers.php
@@ -280,6 +280,7 @@ echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/m
 	/*if $FromCriteria is not set then show a form to allow input	*/
 
 		echo "<form sction='" . $_SERVER['PHP_SELF'] . '?' . SID . "' method='POST'><table>";
+		echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 		echo '<tr><td>' . _('From Supplier Code') . ":</font></td>
 			<td><input tabindex='1' type=text maxlength=6 size=7 name=FromCriteria value='1'></td>
