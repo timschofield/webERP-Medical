0af5f6dfa4b4d79f463688ee2a5e198f112688a7
Tim Schofield <tim@weberpafrica.com>
2010-09-30 15:47:10 +0100

    Add form verification to prevent form spoofing

--- reportwriter/install/ReportListForm.php
+++ reportwriter/install/ReportListForm.php
@@ -1,6 +1,6 @@
 <?PHP
 /* This function retrieves the reports given a certain group id as defined in /reports/admin/defaults.php
-in the acssociative array $ReportGroups[]. It will fetch the reports belonging solely to the group 
+in the acssociative array $ReportGroups[]. It will fetch the reports belonging solely to the group
 specified to create a select form to choose a report. Two on-submit select forms will be generated, one
 for default reports and the other for custom reports.
 
@@ -13,11 +13,12 @@ function GetReports($GroupID) {
 	global $db, $rootpath;
 	$Title= array(_('Custom Reports'), _('Default Reports'));
 	$RptForm = '<form name="ReportList" method="post" action="'.$rootpath.'/reportwriter/ReportMaker.php?action=go">';
+	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 	$RptForm .= '<table align="center" border="0" cellspacing="1" cellpadding="1" class="table_index">';
 	for ($Def=1; $Def>=0; $Def--) {
 		$RptForm .= '<tr><td colspan="2"><div align="center">'.$Title[$Def].'</div></td></tr>';
-		$sql= "SELECT id, reportname FROM reports 
-			WHERE defaultreport='".$Def."' AND groupname='".$GroupID."' 
+		$sql= "SELECT id, reportname FROM reports
+			WHERE defaultreport='".$Def."' AND groupname='".$GroupID."'
 			ORDER BY reportname";
 		$Result=DB_query($sql,$db,'','',false,true);
 		if (DB_num_rows($Result)>0) {
