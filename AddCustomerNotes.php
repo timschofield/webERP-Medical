f9605ddc4a2576511b0dd818e62d2f44ed2bf610
Tim Schofield <tim@weberpafrica.com>
2010-09-30 11:27:04 +0100

    Add form verification to prevent form spoofing

--- AddCustomerNotes.php
+++ AddCustomerNotes.php
@@ -147,6 +147,7 @@ echo '<p>';
 if (!isset($_GET['delete'])) {
 
 	echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '?' . SID . '&DebtorNo='.$DebtorNo.'">';
+	echo '<br><form method="post" id="AccountGroups" action="' . $_SERVER['PHP_SELF'] . '?' . SID . '">';
 
 	if (isset($Id)) {
 		//editing an existing
