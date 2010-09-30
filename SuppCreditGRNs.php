fb13451b59fa95ff74b791667281330152a1f10f
Tim Schofield <tim@weberpafrica.com>
2010-09-30 14:18:47 +0100

    Add form verification to prevent form spoofing

--- SuppCreditGRNs.php
+++ SuppCreditGRNs.php
@@ -136,6 +136,7 @@ if (DB_num_rows($GRNResults)==0){
 
 /*Set up a table to show the GRNs outstanding for selection */
 echo '<form action="' . $_SERVER['PHP_SELF'] . '?' . SID . '" method=post>';
+echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 echo '<br /><table cellpadding=2 colspan=7 class=selection>';
 
