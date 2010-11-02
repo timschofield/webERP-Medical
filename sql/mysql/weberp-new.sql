739af170ad5a6d145b7a4ab4f50afc13f6fddd19
apmuthu <>
2010-11-02 13:40:16 +0000

    Restored redundant config value last sql line - in case the demo has been messed up - thanks Phil

--- sql/mysql/weberp-new.sql
+++ sql/mysql/weberp-new.sql
@@ -3234,3 +3234,4 @@ INSERT INTO `securityroles` VALUES (9,'Supplier Log On Only');
 SET FOREIGN_KEY_CHECKS = 1;
 UPDATE systypes SET typeno=0;
 INSERT INTO shippers VALUES (1,'Default Shipper',0);
+UPDATE config SET confvalue='1' WHERE confname='Default_Shipper';
