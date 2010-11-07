ef616a38f2067ce2ef65a70172cd99dfa0a8315c
turbopt <>
2010-11-07 02:24:38 +0000

    06/11/10 Paul T: InputSerialItemsSequential.php - Bug# 3080130 - Add new FormID to form. (and minor cleanup)

--- includes/InputSerialItemsSequential.php
+++ includes/InputSerialItemsSequential.php
@@ -19,32 +19,33 @@ if (isset($_GET['LineNo'])){
 	$LineNo = $_POST['LineNo'];
 }
 
-echo '<TD valign=top>';
+echo '<td valign="top">';
 
 /*Start a new table for the Serial/Batch ref input  in one column (as a sub table
 then the multi select box for selection of existing bundle/serial nos for dispatch if applicable*/
-//echo '<TABLE><TR><TD valign=TOP>';
+//echo '<table><tr><td valign="top">';
 
 /*in the first column add a table for the input of newies */
-echo '<TABLE>';
+echo '<table>';
 echo $tableheader;
 
 
-echo '<FORM ACTION="' . $_SERVER['PHP_SELF'] . '?=' . $SID . '" METHOD="POST">
-      <input type=hidden name=LineNo value="' . $LineNo . '">
-      <input type=hidden name=StockID value="' . $StockID . '">
-      <input type=hidden name=EntryType value="SEQUENCE">';
+echo '<form action="' . $_SERVER['PHP_SELF'] . '?=' . $SID . '" method="post">
+      <input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />
+      <input type="hidden" name="LineNo" value="' . $LineNo . '">
+      <input type="hidden" name="StockID" value="' . $StockID . '">
+      <input type="hidden" name="EntryType" value="SEQUENCE">';
 if ( isset($_GET['EditControlled']) ) {
 	$EditControlled = isset($_GET['EditControlled'])?$_GET['EditControlled']:false;
 } elseif ( isset($_POST['EditControlled']) ){
 	$EditControlled = isset($_POST['EditControlled'])?$_POST['EditControlled']:false;
 }
-echo '<TR><TD valign=top>'. _('Begin:') . '</td><td> <input type=text name="BeginNo" size=21  maxlength=20 value="'. $_POST['BeginNo']. '"></td></tr>';
-echo '<TR><TD valign=top>'. _('End:') . '</td><td> <input type=text name="EndNo" size=21  maxlength=20  value="'. $_POST['EndNo']. '"></td></tr>';
+echo '<tr><td valign="top">'. _('Begin:') . '</td><td> <input type="text" name="BeginNo" size="21"  maxlength="20" value="'. $_POST['BeginNo']. '"></td></tr>';
+echo '<tr><td valign="top">'. _('End:') . '</td><td> <input type="text" name="EndNo" size="21"  maxlength="20"  value="'. $_POST['EndNo']. '"></td></tr>';
 
 
 echo '</table>';
-echo '<br><center><INPUT TYPE=SUBMIT NAME="AddSequence" VALUE="'. _('Enter'). '"></center><BR>';
-echo '</FORM></TD><TD valign=top>';
-//echo '</TD></TR></TABLE>'; /*end of nested table */
+echo '<br><center><input type="submit" name="AddSequence" value="'. _('Enter'). '"></center><br>';
+echo '</form></td><td valign="top">';
+//echo '</td></tr></table>'; /*end of nested table */
 ?>
