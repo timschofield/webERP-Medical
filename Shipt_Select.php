54737c8264a2127bc2aa24af46fa3ca5ae26896d
tim_schofield <>
2010-12-22 16:17:27 +0000

    $Message

--- Shipt_Select.php
+++ Shipt_Select.php
@@ -8,7 +8,7 @@ include('includes/session.inc');
 $title = _('Search Shipments');
 include('includes/header.inc');
 echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/magnifier.png" title="' . _('Search') .
-	'" alt="">' . ' ' . $title . '</p>';
+	'" alt="" />' . ' ' . $title . '</p>';
 
 if (isset($_GET['SelectedStockItem'])){
 	$SelectedStockItem=$_GET['SelectedStockItem'];
