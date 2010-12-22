ad4671e7a34791abf80450fb28131004282d1e05
tim_schofield <>
2010-12-22 15:56:15 +0000

    $Message

--- CustomerTypes.php
+++ CustomerTypes.php
@@ -20,7 +20,8 @@ if (isset($Errors)) {
 
 $Errors = array();
 
-echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' . _('Customer Types') . '" alt="">' . _('Customer Type Setup') . '</p>';
+echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' . _('Customer Types') .
+	'" alt="" />' . _('Customer Type Setup') . '</p>';
 echo '<div class="page_help_text">' . _('Add/edit/delete Customer Types') . '</div>';
 
 if (isset($_POST['submit'])) {
