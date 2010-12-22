ad266e5615cb9c5211a70b3c2d22c8dcc1cad0df
tim_schofield <>
2010-12-22 15:51:38 +0000

    $Message

--- BOMExtendedQty.php
+++ BOMExtendedQty.php
@@ -282,7 +282,8 @@ if (isset($_POST['PrintPDF'])) {
 
 	$title=_('Quantity Extended BOM Listing');
 	include('includes/header.inc');
-	echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' . _('Search') . '" alt="">' . ' ' . $title.'<br>';
+	echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' .
+		_('Search') . '" alt="" />' . ' ' . $title.'</p><br />';
 
 	echo '</br></br><form action=' . $_SERVER['PHP_SELF'] . " method='post'><table class=selection>";
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
