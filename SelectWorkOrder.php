5423b65be482c5186b8f230c8450e6d9b387098c
tim_schofield <>
2010-12-22 16:16:42 +0000

    $Message

--- SelectWorkOrder.php
+++ SelectWorkOrder.php
@@ -8,7 +8,7 @@ include('includes/session.inc');
 $title = _('Search Work Orders');
 include('includes/header.inc');
 
-echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/magnifier.png" title="' . _('Search') . '" alt="">' . ' ' . $title;
+echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/magnifier.png" title="' . _('Search') . '" alt="" />' . ' ' . $title . '</p>';
 echo '<form action=' . $_SERVER['PHP_SELF'] .'?' .SID . ' method=post>';
 echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
