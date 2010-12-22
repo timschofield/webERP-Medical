f96e6d7a38c3b86dc02af2fb48c2f333bba8fe25
tim_schofield <>
2010-12-22 16:04:53 +0000

    $Message

--- MRPShortages.php
+++ MRPShortages.php
@@ -246,7 +246,7 @@ if (isset($_POST['PrintPDF'])) {
 	include('includes/header.inc');
 
 	echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/inventory.png" title="'
-		. _('Stock') . '" alt="">' . ' ' . $title . '</p>';
+		. _('Stock') . '" alt="" />' . ' ' . $title . '</p>';
 
 	echo '<form action=' . $_SERVER['PHP_SELF'] . " method='post'>";
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
