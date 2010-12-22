db25135fc5d8ac9354a24a11f212bb66ec27d478
tim_schofield <>
2010-12-22 16:19:05 +0000

    $Message

--- StockLocMovements.php
+++ StockLocMovements.php
@@ -13,7 +13,7 @@ echo '<form action="' . $_SERVER['PHP_SELF'] . '?' . SID . '" method=post>';
 echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 echo '<p class="page_title_text"><img src="' . $rootpath . '/css/' . $theme . '/images/magnifier.png" title="' . _('Search') .
-	'" alt="">' . ' ' . $title.'</p>';
+	'" alt="" />' . ' ' . $title.'</p>';
 
 echo '<table class=selection><tr><td>';
 echo '  ' . _('From Stock Location') . ':<select name="StockLocation"> ';
