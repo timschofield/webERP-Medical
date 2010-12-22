d3652908a47c0223026af63cb41bf16c18f19d78
tim_schofield <>
2010-12-22 15:51:47 +0000

    $Message

--- BOMIndented.php
+++ BOMIndented.php
@@ -258,7 +258,8 @@ if (isset($_POST['PrintPDF'])) {
 
 	$title=_('Indented BOM Listing');
 	include('includes/header.inc');
-        echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' . _('Search') . '" alt="">' . ' ' . $title.'<br>';
+        echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' .
+			_('Search') . '" alt="" />' . ' ' . $title.'</p><br />';
 
 	echo '</br></br><form action=' . $_SERVER['PHP_SELF'] . " method='post'><table class=selection>";
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
