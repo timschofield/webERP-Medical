0735a267bee53140f250cbe24e4ee609968f57fd
Tim Schofield <tim@weberpafrica.com>
2011-01-08 14:59:09 +0000

    Correct the quotes in the sql literals

--- Z_CurrencyDebtorsBalances.php
+++ Z_CurrencyDebtorsBalances.php
@@ -8,12 +8,12 @@ include('includes/header.inc');
 
 echo '<font size=4><b>' . _('Debtors Balances By Currency Totals') . '</b></font>';
 
-$sql = 'SELECT SUM(ovamount+ovgst+ovdiscount+ovfreight-alloc) AS currencybalance,
+$sql = "SELECT SUM(ovamount+ovgst+ovdiscount+ovfreight-alloc) AS currencybalance,
 		currcode,
 		SUM((ovamount+ovgst+ovdiscount+ovfreight-alloc)/rate) AS localbalance
 	FROM debtortrans INNER JOIN debtorsmaster
 		ON debtortrans.debtorno=debtorsmaster.debtorno
-	WHERE (ovamount+ovgst+ovdiscount+ovfreight-alloc)<>0 GROUP BY currcode';
+	WHERE (ovamount+ovgst+ovdiscount+ovfreight-alloc)<>0 GROUP BY currcode";
 
 $result = DB_query($sql,$db);
 
