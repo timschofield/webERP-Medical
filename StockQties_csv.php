dac7644a412ac41ee21f24a49d86be47a17f6f5f
Tim Schofield <tim@weberpafrica.com>
2011-01-08 16:59:03 +0000

    Correct the quotes in the sql literals

--- StockQties_csv.php
+++ StockQties_csv.php
@@ -18,7 +18,7 @@ echo '<div class=centre>' . _('Making a comma separated values file of the curre
 
 $ErrMsg = _('The SQL to get the stock quantities failed with the message');
 
-$sql = 'SELECT stockid, SUM(quantity) FROM locstock GROUP BY stockid HAVING SUM(quantity)<>0';
+$sql = "SELECT stockid, SUM(quantity) FROM locstock GROUP BY stockid HAVING SUM(quantity)<>0";
 $result = DB_query($sql, $db, $ErrMsg);
 
 if (!file_exists($_SESSION['reports_dir'])){
