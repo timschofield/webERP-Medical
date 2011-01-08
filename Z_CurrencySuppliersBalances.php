b3adba3702eede2ca6dfea771640791b9eb0421f
Tim Schofield <tim@weberpafrica.com>
2011-01-08 14:58:49 +0000

    Correct the quotes in the sql literals

--- Z_CurrencySuppliersBalances.php
+++ Z_CurrencySuppliersBalances.php
@@ -8,11 +8,11 @@ include('includes/header.inc');
 
 echo '<font size=4><b>' . _('Suppliers Balances By Currency Totals') . '</b></font>';
 
-$sql = 'SELECT SUM(ovamount+ovgst-alloc) AS currencybalance,
+$sql = "SELECT SUM(ovamount+ovgst-alloc) AS currencybalance,
 		currcode,
 		SUM((ovamount+ovgst-alloc)/rate) AS localbalance
 		FROM supptrans INNER JOIN suppliers ON supptrans.supplierno=suppliers.supplierid
-		WHERE (ovamount+ovgst-alloc)<>0 GROUP BY currcode';
+		WHERE (ovamount+ovgst-alloc)<>0 GROUP BY currcode";
 
 $result = DB_query($sql,$db);
 
