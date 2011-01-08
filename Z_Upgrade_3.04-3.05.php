cf5e27cccbd9f514af867e25909e73b7f8ea3bbd
Tim Schofield <tim@weberpafrica.com>
2011-01-08 14:42:22 +0000

    Correct the quotes in the sql literals

--- Z_Upgrade_3.04-3.05.php
+++ Z_Upgrade_3.04-3.05.php
@@ -63,7 +63,7 @@ if ($_POST['DoUpgrade'] == _('Perform Upgrade')){
 
 	prnMsg(_('Upgrade script to put cost information against GRN records from purchorderdetails records .... please wait'),'info');
 
-	$TestAlreadyDoneResult = DB_query('SELECT * FROM grns WHERE stdcostunit<>0',$db);
+	$TestAlreadyDoneResult = DB_query("SELECT * FROM grns WHERE stdcostunit<>0",$db);
 	if (DB_num_rows($TestAlreadyDoneResult)>0){
 		prnMsg(_('The upgrade script appears to have been run already successfully - there is no need to re-run it'),'info');
 		include('includes/footer.inc');
@@ -71,7 +71,7 @@ if ($_POST['DoUpgrade'] == _('Perform Upgrade')){
 	}
 
 
-	$UpdateGRNCosts = DB_query('UPDATE grns INNER JOIN purchorderdetails ON grns.podetailitem=purchorderdetails.podetailitem SET grns.stdcostunit = purchorderdetails.stdcostunit', $db);
+	$UpdateGRNCosts = DB_query("UPDATE grns INNER JOIN purchorderdetails ON grns.podetailitem=purchorderdetails.podetailitem SET grns.stdcostunit = purchorderdetails.stdcostunit", $db);
 
 
 	prnMsg(_('The GRN records have been updated with cost information from purchorderdetails successfully'),'success');
