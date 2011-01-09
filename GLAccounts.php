b0fef7b342c9d2b5c0ad4f7c3f2bc6ab7ced098b
Tim Schofield <tim@weberpafrica.com>
2011-01-09 09:46:38 +0000

    Correct the quotes in the sql literals

--- GLAccounts.php
+++ GLAccounts.php
@@ -244,7 +244,7 @@ if (!isset($_GET['delete'])) {
 	if (!isset($_POST['AccountName'])) {$_POST['AccountName']='';}
 	echo '<tr><td>' . _('Account Name') . ":</td><td><input type='Text' size=51 maxlength=50 name='AccountName' value='" . $_POST['AccountName'] . "'></td></tr>";
 
-	$sql = 'SELECT groupname FROM accountgroups ORDER BY sequenceintb';
+	$sql = "SELECT groupname FROM accountgroups ORDER BY sequenceintb";
 	$result = DB_query($sql, $db);
 
 	echo '<tr><td>' . _('Account Group') . ':</td><td><select name=Group>';
