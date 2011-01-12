74c899713e3ef124ec58d154641c2dd5a663afc8
Tim Schofield <tim@weberpafrica.com>
2011-01-12 10:37:43 +0000

    Corrections on upgrade script to properly make sql file

--- sql/mysql/updates/57.php
+++ sql/mysql/updates/57.php
@@ -4,7 +4,9 @@
  * order line record
  */
 
-$sql="SELECT podetailitem,
+if (!isset($SQLFile)) {
+
+	$sql="SELECT podetailitem,
 			itemcode,
 			purchorders.supplierno
 		FROM purchorderdetails
@@ -12,26 +14,27 @@ $sql="SELECT podetailitem,
 			ON purchorders.orderno=purchorderdetails.orderno
 		WHERE conversionfactor=0";
 
-$result=DB_query($sql, $db);
+	$result=DB_query($sql, $db);
 
-while ($myrow=DB_fetch_array($result)) {
-	$ConversionFactorSQL="SELECT conversionfactor
+	while ($myrow=DB_fetch_array($result)) {
+		$ConversionFactorSQL="SELECT conversionfactor
 							FROM purchdata
 							WHERE supplierno='".$myrow['supplierno'] . "'
 								AND stockid='".$myrow['itemcode']."'";
-	$ConversionFactorResult=DB_query($ConversionFactorSQL, $db);
-	if (DB_num_rows($ConversionFactorResult)>0) {
-		$ConversionFactorRow=DB_fetch_array($ConversionFactorResult);
-		$ConversionFactor=$ConversionFactorRow['conversionfactor'];
-	} else {
-		$ConversionFactor=1;
-	}
-	$UpdateSQL="UPDATE purchorderdetails
+		$ConversionFactorResult=DB_query($ConversionFactorSQL, $db);
+		if (DB_num_rows($ConversionFactorResult)>0) {
+			$ConversionFactorRow=DB_fetch_array($ConversionFactorResult);
+			$ConversionFactor=$ConversionFactorRow['conversionfactor'];
+		} else {
+			$ConversionFactor=1;
+		}
+		$UpdateSQL="UPDATE purchorderdetails
 				SET conversionfactor='".$ConversionFactor."'
 				WHERE podetailitem='".$myrow['podetailitem']."'";
-	$UpdateResult=DB_query($UpdateSQL, $db);
+		$UpdateResult=executeSQL($UpdateSQL, $db);
+	}
+	OutputResult( _('Purchase order details have been correctly updated'), 'success');
 }
-OutputResult( _('Purchase order details have been correctly updated'), 'success');
 
 UpdateDBNo(57, $db);
 
