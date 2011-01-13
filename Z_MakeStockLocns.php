e104bc06c1c31dae49d79779d9caa118e69711ab
Tim Schofield <tim@weberpafrica.com>
2011-01-13 15:16:42 +0000

    Correct the quotes in the sql literals

--- Z_MakeStockLocns.php
+++ Z_MakeStockLocns.php
@@ -11,14 +11,14 @@ include('includes/header.inc');
 
 echo '<br><br>' . _('This script makes stock location records for parts where they do not already exist');
 
-$sql = 'INSERT INTO locstock (stockid, loccode)
+$sql = "INSERT INTO locstock (stockid, loccode)
 		SELECT stockmaster.stockid,
 			locations.loccode
 		FROM stockmaster CROSS JOIN locations
 			LEFT JOIN locstock
 				ON stockmaster.stockid = locstock.stockid
 				AND locations.loccode = locstock.loccode
-                WHERE locstock.stockid IS NULL';
+                WHERE locstock.stockid IS NULL";
 
 $ErrMsg = _('The items/locations that need stock location records created cannot be retrieved because');
 $Result = DB_query($sql,$db,$ErrMsg);
