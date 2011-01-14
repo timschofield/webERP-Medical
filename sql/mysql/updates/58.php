1ff18a62ed8df2454e7c8253f6768c051d932f12
Tim Schofield <tim@weberpafrica.com>
2011-01-14 17:00:34 +0000

    Correct version number in db upgrade script number 58

--- sql/mysql/updates/58.php
+++ sql/mysql/updates/58.php
@@ -3,7 +3,7 @@
 /* Update config value and default price end date
  */
 
-$UpdateSQL = "UPDATE config SET confvalue = '4.00 RC1' WHERE confname='VersionNumber'";
+$UpdateSQL = "UPDATE config SET confvalue = '4.00 RC2' WHERE confname='VersionNumber'";
 $UpdateSQL2 = "UPDATE prices SET enddate = '2030-01-01' WHERE enddate = '0000-00-00'";
 $UpdateSQL3 = "ALTER TABLE prices ALTER COLUMN enddate SET DEFAULT '2030-01-01'";
 $UpdateResult=executeSQL($UpdateSQL, $db);
