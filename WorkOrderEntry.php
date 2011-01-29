6fdf4a747e274759561d6d108edff404c1617f38
Tim Schofield <tim@weberpafrica.com>
2011-01-29 11:48:30 +0000

    Correct brokeb links to select work order and create new work order

--- WorkOrderEntry.php
+++ WorkOrderEntry.php
@@ -252,7 +252,7 @@ if (isset($NewItem) AND isset($_POST['WO'])){
 
 if (isset($_POST['submit'])) { //The update button has been clicked
 
-	echo '<div class=centre><a href="' . $_SERVER['PHP_SELF'] . '?' . SID . "'>" . _('Enter a new work order') . '</a>';
+	echo '<div class=centre><a href="' . $_SERVER['PHP_SELF'] . '?' . SID . '">' . _('Enter a new work order') . '</a>';
 	echo '<br><a href="' . $rootpath . '/SelectWorkOrder.php?' . SID . '">' . _('Select an existing work order') . '</a>';
 	echo '<br><a href="'. $rootpath . '/WorkOrderCosting.php?' . SID . '&WO=' .  $_REQUEST['WO'] . '">' . _('Go to Costing'). '</a></div>';
 
