aa5b4495f8ca829c06ac6712f9bcdbeb7449c11d
Phil Daintree <phil@logicworks.co.nz>
2011-04-04 21:25:38 +0100

    Phil: SQL fixing quotes - not many, fixing gettext a few strings

--- GLCodesInquiry.php
+++ GLCodesInquiry.php
@@ -1,9 +1,7 @@
 <?php
 
 /* $Id$*/
-/* $Revision: 1.9 $ */
 
-//$PageSecurity = 8;
 include ('includes/session.inc');
 
 $title = _('GL Codes Inquiry');
@@ -11,22 +9,24 @@ $title = _('GL Codes Inquiry');
 include('includes/header.inc');
 
 $SQL = "SELECT group_,
-		accountcode ,
-		accountname
-	FROM chartmaster INNER JOIN accountgroups ON chartmaster.group_ = accountgroups.groupname
-	ORDER BY sequenceintb,
-		accountcode";
+				accountcode ,
+				accountname
+			FROM chartmaster
+			INNER JOIN accountgroups
+			ON chartmaster.group_ = accountgroups.groupname
+			ORDER BY sequenceintb,
+					accountcode";
 
 $ErrMsg = _('No general ledger accounts were returned by the SQL because');
 $AccountsResult = DB_query($SQL,$db,$ErrMsg);
 
 /*show a table of the orders returned by the SQL */
 
-echo "<table cellpadding=2 colspan=2>
+echo '<table cellpadding=2 colspan=2>
 		<tr>
-			<th>"._('Group')."</font></th>
-			<th>"._('Code')."</font></th>
-			<th>"._('Account Name').'</font></th>
+			<th>'._('Group').'</font></th>
+			<th>'._('Code').'</font></th>
+			<th>'._('Account Name').'</font></th>
 		</tr>';
 
 $j = 1;
@@ -43,18 +43,18 @@ while ($myrow=DB_fetch_array($AccountsResult)) {
        }
 
        if ($myrow['group_']== $ActGrp){
-              printf("<td></td>
+              printf('<td></td>
 	      		<td><font size=2>%s</font></td>
 			<td><font size=2>%s</font></td>
-			</tr>",
+			</tr>',
 			$myrow['accountcode'],
 			$myrow['accountname']);
        } else {
               $ActGrp = $myrow['group_'];
-              printf("<td><font size=2>%s</font></td>
+              printf('<td><font size=2>%s</font></td>
 	      		<td><font size=2>%s</font></td>
 			<td><font size=2>%s</font></td>
-			</tr>",
+			</tr>',
 			$myrow['group_'],
 			$myrow['accountcode'],
 			$myrow['accountname']);
