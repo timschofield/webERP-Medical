236cddb7caea83dd266ad256e62d6e83cb131a62
Phil Daintree <phil@logicworks.co.nz>
2011-04-05 19:59:33 +0100

    Phil: SQL fixing quotes - not many, fixing gettext a few strings

--- includes/GetPaymentMethods.php
+++ includes/GetPaymentMethods.php
@@ -1,6 +1,5 @@
 <?php
 /* $Id$*/
-// I Figure This is the fastest and safest way to load the Sections
 $PaytTypes    = array();
 $ReceiptTypes = array();
 
@@ -11,10 +10,12 @@ $sql = "SELECT paymentname,
 			ORDER by paymentname";
 $PMResult = DB_query($sql,$db);
 while( $PMrow = DB_fetch_array($PMResult) ) {
-	if ($PMrow['paymenttype']==1)
+	if ($PMrow['paymenttype']==1) {
 		$PaytTypes[] = $PMrow['paymentname'];
-	if ($PMrow['receipttype']==1)
+	}
+	if ($PMrow['receipttype']==1) {
 		$ReceiptTypes[] = $PMrow['paymentname'];
+	}
 }
 DB_free_result($PMResult); // no longer needed
 ?>
