7104b0927f673ea087bdb9d46798e10e93d0cfa6
turbopt <>
2011-04-27 00:34:58 +0000

    Insert missing double-quote.

--- TaxCategories.php
+++ TaxCategories.php
@@ -228,7 +228,7 @@ if (! isset($_GET['delete'])) {
 	}
 	echo '<tr>
 		<td>' . _('Tax Category Name') . ':' . '</td>
-		<td><input type="Text" name="TaxCategoryName" size=30 maxlength=30 value="' . $_POST['TaxCategoryName'] . '></td>
+		<td><input type="Text" name="TaxCategoryName" size=30 maxlength=30 value="' . $_POST['TaxCategoryName'] . '"></td>
 		</tr>';
 	echo '</table>';
 
