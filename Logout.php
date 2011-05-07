4f3b63df270b93cb73b1f27913489d4ab96a6108
Tim Schofield <tim@weberpafrica.com>
2011-05-07 09:47:13 +0300

    Remove deprecated <label> tags

--- Logout.php
+++ Logout.php
@@ -23,7 +23,7 @@ include('includes/session.inc');
 <?php
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 ?>
-		<label><?php echo _('Thank you for using webERP'); ?></label>
+		<span><?php echo _('Thank you for using webERP'); ?></span>
 	<br />
 	<input class="button" type="submit" value="<?php echo _('Login'); ?>" name="SubmitUser" />
 	</form>
