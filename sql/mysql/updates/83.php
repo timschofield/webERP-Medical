7b5cf8284cc73eb68138f1806131453de6105365
Tim Schofield <tim@weberpafrica.com>
2011-06-07 12:00:45 +0300

    Insert new field into all GL transactions

--- sql/mysql/updates/83.php
+++ sql/mysql/updates/83.php
@@ -1,7 +1,7 @@
 <?php
 
 AddColumn('defaulttag', 'www_users', 'VARCHAR( 20 )', 'NOT NULL', '', 'defaultlocation', $db);
-AddColumn('companytag', 'gltrans', 'VARCHAR( 20 )', 'NOT NULL', '', 'jobref', $db);
+AddColumn('defaulttag', 'gltrans', 'VARCHAR( 20 )', 'NOT NULL', '', 'jobref', $db);
 
 UpdateDBNo(83, $db);
 
