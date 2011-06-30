df1da9013b96f32af12fb118db088d80a3824a1b
Phil Daintree <phil@logicworks.co.nz>
2011-06-30 11:36:39 +0100

    Remove redundant SID from links

--- SuppLoginSetup.php
+++ SuppLoginSetup.php
@@ -20,11 +20,10 @@ $ModuleList = array(_('Orders'),
 					_('Petty Cash'),
 					_('Setup'));
 
-echo '<a href="' . $rootpath . '/SelectSupplier.php?' . SID . '">' . _('Back to Suppliers') . '</a><br />';
+echo '<a href="' . $rootpath . '/SelectSupplier.php?">' . _('Back to Suppliers') . '</a><br />';
 
 echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/supplier.png" title="' . _('Supplier') . '" alt="" />'
 	. ' ' . _('Supplier') . ' : ' . $_SESSION['SupplierID'] . _(' has been selected') . '</p><br />';
-//echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/group_add.png" title="' . _('Search') . '" alt="">' . ' ' . $title.'</p><br />';
 
 
 //Make an array of the security roles where only one role is active and is ID 1
