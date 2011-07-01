52c9a6b48302e37f4e680eeb5a77bae3d6a8a810
Phil Daintree <phil@logicworks.co.nz>
2011-07-01 12:15:33 +0100

    Add language array

---
+++ includes/LanguagesArray.php
@@ -0,0 +1,32 @@
+<?php
+$LanguagesArray = array();
+$LanguagesArray['en_US.utf8'] = _('English US');
+$LanguagesArray['en_GB.utf8'] = _('English British');
+$LanguagesArray['cz_CZ.utf8'] = _('Czech');
+$LanguagesArray['de_DE.utf8'] = _('German');
+$LanguagesArray['el_GR.utf8'] = _('Greek');
+$LanguagesArray['es_ES.utf8'] = _('Spanish');
+$LanguagesArray['et_EE.utf8'] = _('Estonian');
+$LanguagesArray['fa_IR.utf8'] = _('Arabic');
+$LanguagesArray['fr_FR.utf8'] = _('French');
+$LanguagesArray['hi_IN.utf8'] = _('Hindi');
+$LanguagesArray['hr_HR.utf8'] = _('Hungarian');
+$LanguagesArray['id_ID.utf8'] = _('Indonisian');
+$LanguagesArray['it_IT.utf8'] = _('Italian');
+$LanguagesArray['ja_JP.utf8'] = _('Japanese');
+$LanguagesArray['lv_LV.utf8'] = _('Latvian');
+$LanguagesArray['nl_NL.utf8'] = _('Dutch');
+$LanguagesArray['pl_PL.utf8'] = _('Polish');
+$LanguagesArray['pt_BR.utf8'] = _('Brazilian Portuguese');
+$LanguagesArray['pt_PT.utf8'] = _('Portuguese');
+$LanguagesArray['ro_RO.utf8'] = _('Romanian');
+$LanguagesArray['ru_RU.utf8'] = _('Russian');
+$LanguagesArray['sq_AL.utf8'] = _('Albanian');
+$LanguagesArray['sv_SE.utf8'] = _('Swedish');
+$LanguagesArray['sw_KE.utf8'] = _('Swahili');
+$LanguagesArray['tr_TR.utf8'] = _('Turkish');
+$LanguagesArray['vi_VN.utf8'] = _('Vietnamese');
+$LanguagesArray['zh_CN.utf8'] = _('Chinese - Simplified');
+$LanguagesArray['zh_HK.utf8'] = _('Chinese - Traditional');
+asort($LanguagesArray);
+?>
