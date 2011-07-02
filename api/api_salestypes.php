cb91e06a248219d839d4b4ea0bb072f6a55a65c4
Phil Daintree <phil@logicworks.co.nz>
2011-07-02 17:32:14 +0100

    Change the layout of the code

--- api/api_salestypes.php
+++ api/api_salestypes.php
@@ -66,8 +66,8 @@
 			$FieldNames.=$key.', ';
 			$FieldValues.='"'.$value.'", ';
 		}
-		$sql = "INSERT INTO salestypes (".substr($FieldNames,0,-2).") ".
-		  "VALUES (".substr($FieldValues,0,-2).") ";
+		$sql = "INSERT INTO salestypes ('" . substr($FieldNames,0,-2) . "')
+				VALUES ('" . substr($FieldValues,0,-2) . "') ";
 		if (sizeof($Errors)==0) {
 			$result = DB_Query($sql, $db);
 			if (DB_error_no($db) != 0) {
