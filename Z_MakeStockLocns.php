f6d7439a1d913cd00c59a612bb47a2f5f0767ae6
Phil Daintree <phil@logicworks.co.nz>
2011-07-02 12:29:15 +0100

    Change the layout of the code

--- Z_MakeStockLocns.php
+++ Z_MakeStockLocns.php
@@ -2,12 +2,9 @@
 /* $Id$*/
 /* Script to make stock locations for all parts that do not have stock location records set up*/
 
-
-//$PageSecurity=15;
 include ('includes/session.inc');
 $title = _('Make LocStock Records');
 include('includes/header.inc');
-
 
 echo '<br /><br />' . _('This script makes stock location records for parts where they do not already exist');
 
@@ -22,8 +19,6 @@ $sql = "INSERT INTO locstock (stockid, loccode)
 
 $ErrMsg = _('The items/locations that need stock location records created cannot be retrieved because');
 $Result = DB_query($sql,$db,$ErrMsg);
-
-
 
 echo '<br />';
 prnMsg(_('Any stock items that may not have had stock location records have now been given new location stock records'),'info');
