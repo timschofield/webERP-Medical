b5440d26a0aeaffb8a6bc97ac30b45beb27c358b
Tim Schofield <tim@weberpafrica.com>
2011-07-04 17:49:09 +0100

    Add new testing scripts

--- build/check_closing_tags.sh
+++ build/check_closing_tags.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
+tags=('p', 'option', 'table')
+
 ROOT_DIR=$PWD
 cd $ROOT_DIR
 for f in `find . -name "*.php" | sort`
 do
     newname=`echo $f | cut -c3-`
     filename="$ROOT_DIR/$newname"
-	tag='<'$1
-	ctag='</'$1
-    output_opening=`grep $tag -c $filename`
-    output_closing=`grep $ctag -c $filename`
-    if [ $output_opening != $output_closing ]
-    then
-	echo $newname' has '$[$output_opening-$output_closing]' problem lines'
-    fi
+	for tagname in "${tags[@]}"
+	do
+		tag='<'$tagname
+		ctag='</'$tagname
+		output_opening=`grep $tag -c $filename`
+		output_closing=`grep $ctag -c $filename`
+		if [ $output_opening != $output_closing ]
+		then
+			echo $newname' has '$[$output_opening-$output_closing]' problem lines for tag '$tagname
+		fi
+	done
 done
 
 for f in `find . -name "*.inc" | sort`
 do
     newname=`echo $f | cut -c3-`
     filename="$ROOT_DIR/$newname"
-
-    output_opening=`grep $tag -c $filename`
-    output_closing=`grep $ctag -c $filename`
-    if [ $output_opening != $output_closing ]
-    then
-	echo $newname' has '$[$output_opening-$output_closing]' problem lines'
-    fi
+	for tagname in "${tags[@]}"
+	do
+		tag='<'$tagname
+		ctag='</'$tagname
+		output_opening=`grep $tag -c $filename`
+		output_closing=`grep $ctag -c $filename`
+		if [ $output_opening != $output_closing ]
+		then
+			echo $newname' has '$[$output_opening-$output_closing]' problem lines for tag '$tagname
+		fi
+	done
 done
