49fcc344cf8e5b5d026d57b0169f56946cefa3b7
Tim Schofield <tim@weberpafrica.com>
2011-07-14 14:37:19 +0100

    XHTML quoting corrections

--- Manufacturers.php
+++ Manufacturers.php
@@ -155,7 +155,7 @@ if (!isset($ManufacturerID)) {
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 	echo '<input type="hidden" name="New" value="No">';
-	echo '<table class=selection><tr>';
+	echo '<table class="selection"><tr>';
 	echo '<th>' . _('ID') . '</th>';
 	echo '<th>' . _('Company Name').'</th>';
 	echo '<th>' . _('Address 1').'</th>';
@@ -265,7 +265,7 @@ if (isset($_POST['amend']) or isset($_POST['Create'])) {
 	echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">';
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 	echo '<input type=hidden name="ManufacturerID" value="'.$ManufacturerID.'">';
-	echo '<table class=selection>';
+	echo '<table class="selection">';
 	echo '<input type=hidden name="New" value="Yes">';
 	echo '<tr><td>' . _('Manufacturer Name') . ':</td><td><input tabindex=1 type="text" name="ManufacturerName" size=42 maxlength=40 value="'.$_POST['ManufacturerName'].'"></td></tr>';
 	echo '<tr><td>' . _('Address Line 1') . ':</td><td><input tabindex=2 type="text" name="Address1" size=42 maxlength=40 value="'.$_POST['Address1'].'"></td></tr>';
