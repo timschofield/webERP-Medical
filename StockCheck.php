903bf5a84330eeda1efb5fd35d3601b8dc222afd
Tim Schofield <tim@weberpafrica.com>
2011-07-14 14:22:22 +0100

    XHTML quoting corrections

--- StockCheck.php
+++ StockCheck.php
@@ -263,7 +263,7 @@ If (isset($_POST['PrintPDF'])
 		echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/printer.png" title="'
 			. _('print') . '" alt="" />' . ' ' . $title.'</p><br />';
 
-		echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="POST"><table class=selection>';
+		echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="POST"><table class="selection">';
 		echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 		echo '<tr><td>' . _('From Inventory Category Code') . ':</font></td><td><select name="FromCriteria">';
