a23024c4b0630da52b70ac545399fa6a6cdcdfc7
Tim Schofield <tim@weberpafrica.com>
2011-07-14 22:41:27 +0100

    XHTML quoting corrections

--- Z_CurrencySuppliersBalances.php
+++ Z_CurrencySuppliersBalances.php
@@ -5,7 +5,7 @@ include('includes/session.inc');
 $title=_('Currency Debtor Balances');
 include('includes/header.inc');
 
-echo '<font size=4><b>' . _('Suppliers Balances By Currency Totals') . '</b></font>';
+echo '<font size="4"><b>' . _('Suppliers Balances By Currency Totals') . '</b></font>';
 
 $sql = "SELECT SUM(ovamount+ovgst-alloc) AS currencybalance,
 		currcode,
@@ -17,20 +17,20 @@ $result = DB_query($sql,$db);
 
 $LocalTotal =0;
 
-echo '<table>';
+echo '<table class="selection">';
 
 while ($myrow=DB_fetch_array($result)){
 
-	echo '<tr><td><font size=4>' . _('Total Supplier Balances in') . ' </font></td>
-		<td><font size=4>' . $myrow['currcode'] . '</font></td>
-		<td class=number><font size=4>' . number_format($myrow['currencybalance'],2) . '</font></td>
-		<td><font size=4> ' . _('in') . ' ' . $_SESSION['CompanyRecord']['currencydefault'] . '</font></td>
-		<td class=number><font size=4>' . number_format($myrow['localbalance'],2) . '</font></td></tr>';
+	echo '<tr><td><font size="4">' . _('Total Supplier Balances in') . ' </font></td>
+		<td><font size="4">' . $myrow['currcode'] . '</font></td>
+		<td class="number"><font size="4">' . number_format($myrow['currencybalance'],2) . '</font></td>
+		<td><font size="4"> ' . _('in') . ' ' . $_SESSION['CompanyRecord']['currencydefault'] . '</font></td>
+		<td class="number"><font size="4">' . number_format($myrow['localbalance'],2) . '</font></td></tr>';
 	$LocalTotal += $myrow['localbalance'];
 }
 
-echo '<tr><td colspan=4><font size=4>' . _('Total Balances in local currency') . ':</font></td>
-	<td class=number><font size=4>' . number_format($LocalTotal,2) . '</font></td></tr>';
+echo '<tr><td colspan="4"><font size="4">' . _('Total Balances in local currency') . ':</font></td>
+	<td class="number"><font size="4">' . number_format($LocalTotal,2) . '</font></td></tr>';
 
 echo '</table>';
 
