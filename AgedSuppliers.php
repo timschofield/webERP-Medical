7c91d15bda8bda157b0627dc9b26edf3e46509ff
Tim Schofield <tim@weberpafrica.com>
2011-07-15 23:15:14 +0100

    XHTML quoting corrections

--- AgedSuppliers.php
+++ AgedSuppliers.php
@@ -282,12 +282,12 @@ echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/m
 		echo '<form sction="' . $_SERVER['PHP_SELF'] . '" method="POST"><table>';
 		echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
-		echo '<tr><td>' . _('From Supplier Code') . ":</font></td>
-			<td><input tabindex='1' type=text maxlength=6 size=7 name=FromCriteria value='1'></td>
-		</tr>";
-		echo '<tr><td>' . _('To Supplier Code') . ":</td>
-			<td><input tabindex='2' Type=text maxlength=6 size=7 name=ToCriteria value='zzzzzz'></td>
-		</tr>";
+		echo '<tr><td>' . _('From Supplier Code') . ':</font></td>
+			<td><input tabindex="1" type="text" maxlength=6 size=7 name=FromCriteria value="1"></td>
+		</tr>';
+		echo '<tr><td>' . _('To Supplier Code') . ':</td>
+			<td><input tabindex="2" type="text" maxlength=6 size=7 name=ToCriteria value="zzzzzz"></td>
+		</tr>';
 
 		echo '<tr><td>' . _('All balances or overdues only') . ':' . "</td>
 			<td><select tabindex='3' name='All_Or_Overdues'>";
