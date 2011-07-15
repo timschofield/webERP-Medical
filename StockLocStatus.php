e013866a86bc5ba3678de94d321135c12e4246a2
Tim Schofield <tim@weberpafrica.com>
2011-07-15 12:23:24 +0100

    XHTML quoting corrections

--- StockLocStatus.php
+++ StockLocStatus.php
@@ -251,11 +251,11 @@ if (isset($_POST['ShowStatus'])){
 				}
 				printf('<td><a target="_blank" href="StockStatus.php?StockID=%s">%s</a></td>
 					<td>%s</td>
-					<td class=number>%s</td>
-					<td class=number>%s</td>
-					<td class=number>%s</td>
-					<td class=number><a target="_blank" href="SelectProduct.php?StockID=%s">%s</a></td>
-					<td class=number>%s</td></tr>',
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number"><a target="_blank" href="SelectProduct.php?StockID=%s">%s</a></td>
+					<td class="number">%s</td></tr>',
 					mb_strtoupper($myrow['stockid']),
 					mb_strtoupper($myrow['stockid']),
 					$myrow['description'],
@@ -281,11 +281,11 @@ if (isset($_POST['ShowStatus'])){
 				}
 				printf('<td><a target="_blank" href="StockStatus.php?StockID=%s">%s</a></td>
 					<td>%s</td>
-					<td class=number>%s</td>
-					<td class=number>%s</td>
-					<td class=number>%s</td>
-					<td class=number><a target="_blank" href="SelectProduct.php?StockID=%s">%s</a></td>
-					<td class=number>%s</td>',
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number"><a target="_blank" href="SelectProduct.php?StockID=%s">%s</a></td>
+					<td class="number">%s</td>',
 					mb_strtoupper($myrow['stockid']),
 					mb_strtoupper($myrow['stockid']),
 					$myrow['description'],
