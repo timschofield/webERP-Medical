3a969767a64dff59b41f98e5e40166b4601b69e7
Tim Schofield <tim@weberpafrica.com>
2011-07-16 13:43:56 +0100

    XHTML quoting corrections

--- includes/OutputSerialItems.php
+++ includes/OutputSerialItems.php
@@ -201,7 +201,7 @@ while ($myrow=DB_fetch_array($result)){
 }
 
 echo '</table>';
-echo '<br /><div class=centre><input type=submit name="AddBatches" value="'. _('Enter'). '"></div>';
+echo '<br /><div class="centre"><input type=submit name="AddBatches" value="'. _('Enter'). '"></div>';
 echo '</form></td><td valign=top>';
 $ShowExisting=True;
 $_POST['EntryType']='Sequential';
