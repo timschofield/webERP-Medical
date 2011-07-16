b3cfc92b95d5a8b800fdbbb36d341f6a16b6d114
Tim Schofield <tim@weberpafrica.com>
2011-07-16 09:43:37 +0100

    XHTML quoting corrections

--- includes/InputSerialItemsExisting.php
+++ includes/InputSerialItemsExisting.php
@@ -29,10 +29,10 @@ if ($_POST['EntryType'] == 'KEYED'){
 
 		echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="POST">';
 		echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
-        echo '<input type=hidden name=LineNo value="' . $LineNo . '">
-                        <input type=hidden name=StockID value="' . $StockID . '">
-                        <input type=hidden name=EntryType value="KEYED">
-			<input type=hidden name=EditControlled value="true">
+        echo '<input type="hidden" name=LineNo value="' . $LineNo . '">
+                        <input type="hidden" name=StockID value="' . $StockID . '">
+                        <input type="hidden" name=EntryType value="KEYED">
+			<input type="hidden" name="EditControlled" value="true" />
 			<select Name=Bundles[] multiple>';
 
                 $id=0;
