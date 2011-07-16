108021949617f9958e3c1caedcb04a2c7f84f3d4
Tim Schofield <tim@weberpafrica.com>
2011-07-16 14:39:14 +0100

    XHTML quoting corrections

--- SuppPaymentRun.php
+++ SuppPaymentRun.php
@@ -348,8 +348,8 @@ Payment types can be modified by editing that file */
 	echo '<tr><td>' . _('Starting Reference no (eg chq no)') . ':</td>
             <td><input type="text" name="Ref" maxlength=11 size=12 value=' . $_POST['Ref'] . '></td></tr>';
 
-	echo '</table><br /><div class="centre"><input type=Submit Name="PrintPDF" Value="' . _('Print PDF Only') . '">
-                <input type=Submit Name="PrintPDFAndProcess" Value="' . _('Print and Process Payments') . '"></div>';
+	echo '</table><br /><div class="centre"><input type="submit" Name="PrintPDF" Value="' . _('Print PDF Only') . '">
+                <input type="submit" name="PrintPDFAndProcess" value="' . _('Print and Process Payments') . '" /></div>';
 
 	include ('includes/footer.inc');
 } /*end of else not PrintPDF */
