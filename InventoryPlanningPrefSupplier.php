5e007b15d03dcc01f98c96857b8d55a32fc6c82d
Tim Schofield <tim@weberpafrica.com>
2011-07-16 17:37:52 +0100

    XHTML quoting corrections

--- InventoryPlanningPrefSupplier.php
+++ InventoryPlanningPrefSupplier.php
@@ -515,7 +515,7 @@ if (isset($_POST['PrintPDF'])){
 	}
 	echo '</select></td></tr>';
 
-	echo '</table><br /><div class="centre"><input type=submit Name="PrintPDF" Value="' . _('Print PDF') . '"></div>';
+	echo '</table><br /><div class="centre"><input type="submit" name="PrintPDF" value="' . _('Print PDF') . '" /></div>';
 
 	include('includes/footer.inc');
 } /*end of else not PrintPDF */
