4d38a936c2a414e9dfa6e1fb48cc277f200de600
Tim Schofield <tim@weberpafrica.com>
2011-07-16 14:54:04 +0100

    XHTML quoting corrections

--- Shipments.php
+++ Shipments.php
@@ -424,7 +424,7 @@ if (count($_SESSION['Shipment']->LineItems)>0){
 echo '</table>';
 }//there are lines on the shipment
 
-echo '<br /><div class="centre"><input type=submit name="Update" Value="'. _('Update Shipment Details') . '"></div><br />';
+echo '<br /><div class="centre"><input type="submit" name="Update" value="'. _('Update Shipment Details') . '" /></div><br />';
 
 if (!isset($_POST['StockLocation'])) {
 	$_POST['StockLocation'] =$_SESSION['Shipment']->StockLocation;
