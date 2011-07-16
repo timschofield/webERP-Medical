e9411f6df8d6e47116a24f69e14bc455f4a41a75
Tim Schofield <tim@weberpafrica.com>
2011-07-16 10:16:12 +0100

    XHTML quoting corrections

--- Factors.php
+++ Factors.php
@@ -266,7 +266,7 @@ if (isset($_POST['amend']) or isset($_POST['Create'])) {
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 	echo '<input type="hidden" name="FactorID" value="'.$FactorID.'">';
 	echo '<table class="selection">';
-	echo '<input type=hidden name="New" value="Yes">';
+	echo '<input type="hidden" name="New" value="Yes" />';
 	echo '<tr><td>' . _('Factor company Name') . ':</td><td><input tabindex=1 type="text" name="FactorName" size=42 maxlength=40 value="'.$_POST['FactorName'].'"></td></tr>';
 	echo '<tr><td>' . _('Address Line 1') . ':</td><td><input tabindex=2 type="text" name="Address1" size=42 maxlength=40 value="'.$_POST['Address1'].'"></td></tr>';
 	echo '<tr><td>' . _('Address Line 2') . ':</td><td><input tabindex=3 type="text" name="Address2" size=42 maxlength=40 value="'.$_POST['Address2'].'"></td></tr>';
