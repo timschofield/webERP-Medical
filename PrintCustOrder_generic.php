fb1314c2625804e0a6968d5047d5cc1ee38be9fd
Tim Schofield <tim@weberpafrica.com>
2011-07-16 13:40:07 +0100

    XHTML quoting corrections

--- PrintCustOrder_generic.php
+++ PrintCustOrder_generic.php
@@ -11,7 +11,7 @@ include('includes/SQL_CommonFunctions.inc');
 If (!isset($_GET['TransNo']) OR $_GET['TransNo']==""){
         $title = _('Select Order To Print');
         include('includes/header.inc');
-        echo '<div class=centre><br /><br /><br />';
+        echo '<div class="centre"><br /><br /><br />';
         prnMsg( _('Select an Order Number to Print before calling this page') , 'error');
         echo '<br /><br /><br /><table class="table_index"><tr><td class="menu_group_item">
                 <li><a href="'. $rootpath . '/SelectSalesOrder.php?">' . _('Outstanding Sales Orders') . '</a></li>
@@ -62,7 +62,7 @@ $result=DB_query($sql,$db, $ErrMsg);
 if (DB_num_rows($result)==0){
         $title = _('Print Packing Slip Error');
         include('includes/header.inc');
-         echo '<div class=centre><br /><br /><br />';
+         echo '<div class="centre"><br /><br /><br />';
         prnMsg( _('Unable to Locate Order Number') . ' : ' . $_GET['TransNo'] . ' ', 'error');
         echo '<br /><br /><br /><table class="table_index"><tr><td class="menu_group_item">
                 <li><a href="'. $rootpath . '/SelectSalesOrder.php?">' . _('Outstanding Sales Orders') . '</a></li>
