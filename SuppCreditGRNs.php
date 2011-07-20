a3a1cf03783f0861a65abcd7b1e3c140c9c28cb4
Tim Schofield <tim@weberpafrica.com>
2011-07-20 17:09:40 +0100

    XHTML corrections to code

--- SuppCreditGRNs.php
+++ SuppCreditGRNs.php
@@ -134,7 +134,7 @@ if (DB_num_rows($GRNResults)==0){
 }
 
 /*Set up a table to show the GRNs outstanding for selection */
-echo '<form action="' . $_SERVER['PHP_SELF'] . '" method=post>';
+echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">';
 echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 echo '<br /><table cellpadding=2 colspan=7 class="selection">';
