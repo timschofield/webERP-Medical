72ae29a6498572b4565b410089ed0e33ea9aedcc
Phil Daintree <phil@logicworks.co.nz>
2011-07-21 12:36:30 +0100

    Phil/Beth Lesko: Comments not wrapping correctly on quotations - fixed landscape quotation

--- PDFQuotation.php
+++ PDFQuotation.php
@@ -8,69 +8,69 @@ include('includes/SQL_CommonFunctions.inc');
 
 //Get Out if we have no order number to work with
 If (!isset($_GET['QuotationNo']) || $_GET['QuotationNo']==""){
-        $title = _('Select Quotation To Print');
-        include('includes/header.inc');
-        echo '<div class="centre"><br /><br /><br />';
-        prnMsg( _('Select a Quotation to Print before calling this page') , 'error');
-        echo '<br /><br /><br /><table class="table_index"><tr><td class="menu_group_item">
-                <li><a href="'. $rootpath . '/SelectSalesOrder.php?Quotations=Quotes_Only">' . _('Quotations') . '</a></li>
-                </td></tr></table></div><br /><br /><br />';
-        include('includes/footer.inc');
-        exit();
+		$title = _('Select Quotation To Print');
+		include('includes/header.inc');
+		echo '<div class="centre"><br /><br /><br />';
+		prnMsg( _('Select a Quotation to Print before calling this page') , 'error');
+		echo '<br /><br /><br /><table class="table_index"><tr><td class="menu_group_item">
+				<li><a href="'. $rootpath . '/SelectSalesOrder.php?Quotations=Quotes_Only">' . _('Quotations') . '</a></li>
+				</td></tr></table></div><br /><br /><br />';
+		include('includes/footer.inc');
+		exit();
 }
 
 /*retrieve the order details from the database to print */
 $ErrMsg = _('There was a problem retrieving the quotation header details for Order Number') . ' ' . $_GET['QuotationNo'] . ' ' . _('from the database');
 
 $sql = "SELECT salesorders.customerref,
-		salesorders.comments,
-		salesorders.orddate,
-		salesorders.deliverto,
-		salesorders.deladd1,
-		salesorders.deladd2,
-		salesorders.deladd3,
-		salesorders.deladd4,
-		salesorders.deladd5,
-		salesorders.deladd6,
-		debtorsmaster.name,
-		debtorsmaster.address1,
-		debtorsmaster.address2,
-		debtorsmaster.address3,
-		debtorsmaster.address4,
-		debtorsmaster.address5,
-		debtorsmaster.address6,
-		shippers.shippername,
-		salesorders.printedpackingslip,
-		salesorders.datepackingslipprinted,
-		salesorders.branchcode,
-		locations.taxprovinceid,
-		locations.locationname
-	FROM salesorders,
-		debtorsmaster,
-		shippers,
-		locations
-	WHERE salesorders.debtorno=debtorsmaster.debtorno
-	AND salesorders.shipvia=shippers.shipper_id
-	AND salesorders.fromstkloc=locations.loccode
-	AND salesorders.quotation=1
-	AND salesorders.orderno='" . $_GET['QuotationNo'] ."'";
+				salesorders.comments,
+				salesorders.orddate,
+				salesorders.deliverto,
+				salesorders.deladd1,
+				salesorders.deladd2,
+				salesorders.deladd3,
+				salesorders.deladd4,
+				salesorders.deladd5,
+				salesorders.deladd6,
+				debtorsmaster.name,
+				debtorsmaster.address1,
+				debtorsmaster.address2,
+				debtorsmaster.address3,
+				debtorsmaster.address4,
+				debtorsmaster.address5,
+				debtorsmaster.address6,
+				shippers.shippername,
+				salesorders.printedpackingslip,
+				salesorders.datepackingslipprinted,
+				salesorders.branchcode,
+				locations.taxprovinceid,
+				locations.locationname
+			FROM salesorders,
+				debtorsmaster,
+				shippers,
+				locations
+			WHERE salesorders.debtorno=debtorsmaster.debtorno
+				AND salesorders.shipvia=shippers.shipper_id
+				AND salesorders.fromstkloc=locations.loccode
+				AND salesorders.quotation=1
+				AND salesorders.orderno='" . $_GET['QuotationNo'] ."'";
 
 $result=DB_query($sql,$db, $ErrMsg);
 
 //If there are no rows, there's a problem.
 if (DB_num_rows($result)==0){
-        $title = _('Print Quotation Error');
-        include('includes/header.inc');
-         echo '<div class="centre"><br /><br /><br />';
-        prnMsg( _('Unable to Locate Quotation Number') . ' : ' . $_GET['QuotationNo'] . ' ', 'error');
-        echo '<br /><br /><br /><table class="table_index"><tr><td class="menu_group_item">
-                <li><a href="'. $rootpath . '/SelectSalesOrder.php?Quotations=Quotes_Only">' . _('Outstanding Quotations') . '</a></li>
-                </td></tr></table></div><br /><br /><br />';
-        include('includes/footer.inc');
-        exit;
+		$title = _('Print Quotation Error');
+		include('includes/header.inc');
+		 echo '<div class="centre"><br /><br /><br />';
+		prnMsg( _('Unable to Locate Quotation Number') . ' : ' . $_GET['QuotationNo'] . ' ', 'error');
+		echo '<br /><br /><br /><table class="table_index"><tr><td class="menu_group_item">
+				<li><a href="'. $rootpath . '/SelectSalesOrder.php?Quotations=Quotes_Only">' . _('Outstanding Quotations') . '</a></li>
+				</td></tr></table></div><br /><br /><br />';
+		include('includes/footer.inc');
+		exit;
 } elseif (DB_num_rows($result)==1){ /*There is only one order header returned - thats good! */
 
-        $myrow = DB_fetch_array($result);
+		$myrow = DB_fetch_array($result);
 }
 
 /*retrieve the order details from the database to print */
@@ -93,16 +93,17 @@ $ErrMsg = _('There was a problem retrieving the quotation line details for quota
 	$_GET['QuotationNo'] . ' ' . _('from the database');
 
 $sql = "SELECT salesorderdetails.stkcode,
-		stockmaster.description,
-		salesorderdetails.quantity,
-		salesorderdetails.qtyinvoiced,
-		salesorderdetails.unitprice,
-		salesorderdetails.discountpercent,
-		stockmaster.taxcatid,
-		salesorderdetails.narrative
-	FROM salesorderdetails INNER JOIN stockmaster
-		ON salesorderdetails.stkcode=stockmaster.stockid
-	WHERE salesorderdetails.orderno='" . $_GET['QuotationNo'] . "'";
+				stockmaster.description,
+				salesorderdetails.quantity,
+				salesorderdetails.qtyinvoiced,
+				salesorderdetails.unitprice,
+				salesorderdetails.discountpercent,
+				stockmaster.taxcatid,
+				salesorderdetails.narrative
+			FROM salesorderdetails
+			INNER JOIN stockmaster
+			ON salesorderdetails.stkcode=stockmaster.stockid
+			WHERE salesorderdetails.orderno='" . $_GET['QuotationNo'] . "'";
 
 $result=DB_query($sql,$db, $ErrMsg);
 
@@ -118,7 +119,7 @@ if (DB_num_rows($result)>0){
 
 	while ($myrow2=DB_fetch_array($result)){
 
-        $ListCount ++;
+		$ListCount ++;
 
 		if ((mb_strlen($myrow2['narrative']) >200 AND $YPos-$line_height <= 75)
 			OR (mb_strlen($myrow2['narrative']) >1 AND $YPos-$line_height <= 62)
@@ -137,13 +138,22 @@ if (DB_num_rows($result)>0){
 		$TaxProv = $myrow['taxprovinceid'];
 		$TaxCat = $myrow2['taxcatid'];
 		$Branch = $myrow['branchcode'];
-		$sql3 = " select taxgrouptaxes.taxauthid from taxgrouptaxes INNER JOIN custbranch ON taxgrouptaxes.taxgroupid=custbranch.taxgroupid WHERE custbranch.branchcode='" .$Branch ."'";
+		$sql3 = "SELECT taxgrouptaxes.
+						taxauthid
+					FROM taxgrouptaxes
+					INNER JOIN custbranch
+					ON taxgrouptaxes.taxgroupid=custbranch.taxgroupid
+					WHERE custbranch.branchcode='" .$Branch ."'";
 		$result3=DB_query($sql3,$db, $ErrMsg);
 		while ($myrow3=DB_fetch_array($result3)){
 			$TaxAuth = $myrow3['taxauthid'];
 		}
 
-		$sql4 = "SELECT * FROM taxauthrates WHERE dispatchtaxprovince='" .$TaxProv ."' AND taxcatid='" .$TaxCat ."' AND taxauthority='" .$TaxAuth ."'";
+		$sql4 = "SELECT *
+					FROM taxauthrates
+					WHERE dispatchtaxprovince='" .$TaxProv ."'
+						AND taxcatid='" .$TaxCat ."'
+						AND taxauthority='" .$TaxAuth ."'";
 		$result4=DB_query($sql4,$db, $ErrMsg);
 		while ($myrow4=DB_fetch_array($result4)){
 			$TaxClass = 100 * $myrow4['taxrate'];
@@ -170,10 +180,10 @@ if (DB_num_rows($result)>0){
 		$LeftOvers = $pdf->addTextWrap(700,$YPos,90,$FontSize,$DisplayTotal,'right');
 		if (mb_strlen($myrow2['narrative'])>1){
 			$YPos -= ($line_height);
-			$LeftOvers = $pdf->addTextWrap($XPos+1,$YPos,800,$FontSize,$myrow2['narrative']);
+			$LeftOvers = $pdf->addTextWrap($XPos+1,$YPos,870,$FontSize,$myrow2['narrative']);
 			if (mb_strlen($LeftOvers) >1){
 				$YPos -= 11;
-				$LeftOvers = $pdf->addTextWrap($XPos+1,$YPos,800,$FontSize,$LeftOvers);
+				$LeftOvers = $pdf->addTextWrap($XPos+1,$YPos,870,$FontSize,$LeftOvers);
 			}
 		}
 		$QuotationTotal +=$LineTotal;
@@ -193,25 +203,6 @@ if (DB_num_rows($result)>0){
 			include ('includes/PDFQuotationPageHeader.inc');
 	} //end if need a new page headed up
 
-	$LeftOvers = $pdf->addTextWrap($XPos,$YPos-80,200,10,_('Notes:'));
-	$LeftOvers = $pdf->addText($XPos,$YPos-95,10,$myrow['comments']);
-
-	if (mb_strlen($LeftOvers)>1){
-		$YPos -= 10;
-		$LeftOvers = $pdf->addTextWrap($XPos,$YPos,700,10,$LeftOvers);
-		if (mb_strlen($LeftOvers)>1){
-			$YPos -= 10;
-			$LeftOvers = $pdf->addTextWrap($XPos,$YPos,700,10,$LeftOvers);
-			if (mb_strlen($LeftOvers)>1){
-				$YPos -= 10;
-				$LeftOvers = $pdf->addTextWrap($XPos,$YPos,700,10,$LeftOvers);
-				if (mb_strlen($LeftOvers)>1){
-					$YPos -= 10;
-					$LeftOvers = $pdf->addTextWrap($XPos,$YPos,10,$FontSize,$LeftOvers);
-				}
-			}
-		}
-	}
 	$YPos -= ($line_height);
 	$LeftOvers = $pdf->addTextWrap(40,$YPos,655,$FontSize,_('Total Tax'),'right');
 	$LeftOvers = $pdf->addTextWrap(700,$YPos,90,$FontSize,number_format($TaxTotal,2),'right');
@@ -222,31 +213,38 @@ if (DB_num_rows($result)>0){
 	$LeftOvers = $pdf->addTextWrap(40,$YPos,655,$FontSize,_('Quotation Including Tax'),'right');
 	$LeftOvers = $pdf->addTextWrap(700,$YPos,90,$FontSize,number_format($QuotationTotal,2),'right');
 
+	$YPos -= ($line_height);
+	$LeftOvers = $pdf->addTextWrap($XPos,$YPos,20,10,_('Notes:'));
+	$LeftOvers = $pdf->addTextWrap($XPos+28,$YPos,800,10,$myrow['comments']);
+
+	if (mb_strlen($LeftOvers)>1){
+		$YPos -= 10;
+		$LeftOvers = $pdf->addTextWrap($XPos,$YPos,850,10,$LeftOvers);
+		if (mb_strlen($LeftOvers)>1){
+			$YPos -= 10;
+			$LeftOvers = $pdf->addTextWrap($XPos,$YPos,850,10,$LeftOvers);
+			if (mb_strlen($LeftOvers)>1){
+				$YPos -= 10;
+				$LeftOvers = $pdf->addTextWrap($XPos,$YPos,850,10,$LeftOvers);
+				if (mb_strlen($LeftOvers)>1){
+					$YPos -= 10;
+					$LeftOvers = $pdf->addTextWrap($XPos,$YPos,850,10,$FontSize,$LeftOvers);
+				}
+			}
+		}
+	}
 } /*end if there are line details to show on the quotation*/
 
-
-//$pdfcode = $pdf->output('PDFQuotation.pdf', 'I');
-//$len = mb_strlen($pdfcode);
 if ($ListCount == 0){
-        $title = _('Print Quotation Error');
-        include('includes/header.inc');
-        echo '<br />'. _('There were no items on the quotation') . '. ' . _('The quotation cannot be printed').
-                '<br /><a href="' . $rootpath . '/SelectSalesOrder.php?Quotation=Quotes_only">'. _('Print Another Quotation').
-                '</a>' . '<br />'. '<a href="' . $rootpath . '/index.php">' . _('Back to the menu') . '</a>';
-        include('includes/footer.inc');
+		$title = _('Print Quotation Error');
+		include('includes/header.inc');
+		echo '<br />'. _('There were no items on the quotation') . '. ' . _('The quotation cannot be printed').
+				'<br /><a href="' . $rootpath . '/SelectSalesOrder.php?Quotation=Quotes_only">'. _('Print Another Quotation').
+				'</a>' . '<br />'. '<a href="' . $rootpath . '/index.php">' . _('Back to the menu') . '</a>';
+		include('includes/footer.inc');
 	exit;
 } else {
-/*
-	header('Content-type: application/pdf');
-	header('Content-Length: ' . $len);
-	header('Content-Disposition: inline; filename=Quotation.pdf');
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-	header('Pragma: public');
-//echo 'here';
-	$pdf->Output('PDFQuotation.pdf', 'I');
-*/
-    $pdf->OutputI($_SESSION['DatabaseName'] . '_Quotation_' . date('Y-m-d') . '.pdf');//UldisN
-    $pdf->__destruct(); //UldisN
+	$pdf->OutputI($_SESSION['DatabaseName'] . '_Quotation_' . date('Y-m-d') . '.pdf');//UldisN
+	$pdf->__destruct(); //UldisN
 }
 ?>
