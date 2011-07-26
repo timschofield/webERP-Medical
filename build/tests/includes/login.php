9a4af302f019bf73d50115732ae40ead986f9911
Tim Schofield <tim@weberpafrica.com>
2011-07-26 11:55:55 +0100

    Testing scripts

--- build/tests/includes/login.php
+++ build/tests/includes/login.php
@@ -1,16 +1,56 @@
 <?php
 
-$url=$RootPath.'index.php';
-$fields=array();
-include('GetURL.php');
+function webERPLogIn($ch, $RootPath, $Company, $UserName, $Password) {
+	$TestSessionID = sha1(uniqid(mt_rand(), true));
 
-$FormID=mb_substr(mb_strstr($result, 'FormID" value="', false),15,40);
+	$LoginScreenDetails = new URLDetails($TestSessionID);
+	$LoginScreenDetails->SetURL('index.php');
+	$LoginScreenDetails->SetPostArray(array());
 
-$fields['FormID']=$FormID;
-$fields['CompanyNameField']=$CompanyName;
-$fields['UserNameEntryField']=$UserName;
-$fields['Password']=$Password;
+	$LoginScreenDetails->FetchPage($RootPath, $ch);
 
-include('GetURL.php');
+	for ($i=0; $i<sizeOf($LoginScreenDetails->FormDetails['Selects']['select']['CompanyNameField']['options']); $i++) {
+		if ($LoginScreenDetails->FormDetails['Selects']['select']['CompanyNameField']['options'][$i]['label']==$Company) {
+			$PostArray['CompanyNameField']=$LoginScreenDetails->FormDetails['Selects']['select']['CompanyNameField']['options'][$i]['label'];
+			break;
+		}
+	}
+
+	for ($i=0; $i<sizeOf($LoginScreenDetails->FormDetails['Texts']['text']); $i++) {
+		if ($LoginScreenDetails->FormDetails['Texts']['text'][$i]['name']=='UserNameEntryField') {
+			$PostArray['UserNameEntryField']=$UserName;
+			break;
+		}
+	}
+
+	for ($i=0; $i<sizeOf($LoginScreenDetails->FormDetails['Passwords']['password']); $i++) {
+		if ($LoginScreenDetails->FormDetails['Passwords']['password'][$i]['name']=='Password') {
+			$PostArray['Password']=$Password;
+			break;
+		}
+	}
+
+	for ($i=0; $i<sizeOf($LoginScreenDetails->FormDetails['Hiddens']['hidden']); $i++) {
+		if ($LoginScreenDetails->FormDetails['Hiddens']['hidden'][$i]['name']=='FormID') {
+			$PostArray['FormID']=$LoginScreenDetails->FormDetails['Hiddens']['hidden'][$i]['value'];
+			break;
+		}
+	}
+
+	for ($i=0; $i<sizeOf($LoginScreenDetails->FormDetails['Submits']['submit']); $i++) {
+		if ($LoginScreenDetails->FormDetails['Submits']['submit'][$i]['name']=='SubmitUser') {
+			$PostArray['SubmitUser']='Login';
+			break;
+		}
+	}
+
+	$IndexScreenDetails = new URLDetails($TestSessionID);
+	$IndexScreenDetails->SetURL('index.php');
+	$IndexScreenDetails->SetPostArray($PostArray);
+
+	$IndexScreenDetails->FetchPage($RootPath, $ch);
+
+	return $IndexScreenDetails;
+}
 
 ?>
