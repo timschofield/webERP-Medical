836e37a340cefb41d12acfa34b5d4bc248777727
Tim Schofield <tim@weberpafrica.com>
2011-07-26 13:59:19 +0100

    XHTML corrections to code

--- PDFChequeListing.php
+++ PDFChequeListing.php
@@ -58,7 +58,7 @@ if (!isset($_POST['FromDate']) OR !isset($_POST['ToDate'])){
 	echo '</select></td></tr>';
 
 	echo '<tr><td>' . _('Email the report off') . ':</td><td><select name="Email">';
-	echo '<option selected value="No">' . _('No') . '</option>';
+	echo '<option selected="True" value="No">' . _('No') . '</option>';
 	echo '<option value="Yes">' . _('Yes') . '</option>';
 	echo '</select></td></tr></table><br /><div class="centre"><input type="submit" name="Go" value="' . _('Create PDF') . '" /></div>';
 
