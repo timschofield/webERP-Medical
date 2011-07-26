af21fa68a136d1a084e2e2f78541e42cbc3e6c69
Tim Schofield <tim@weberpafrica.com>
2011-07-26 12:55:39 +0100

    XHTML corrections to code

--- SelectProduct.php
+++ SelectProduct.php
@@ -236,7 +236,7 @@ if (!isset($_POST['Search']) AND (isset($_POST['Select']) OR isset($_SESSION['Se
 				echo '<td align="left" width="60"><select name="PropValue' . $PropertyCounter . '" onChange="ReloadForm(UpdateProps)" >';
 				foreach ($OptionValues as $PropertyOptionValue){
 					if ($PropertyOptionValue == $PropertyValue){
-						echo '<option selected value="' . $PropertyOptionValue . '">' . $PropertyOptionValue . '</option>';
+						echo '<option selected="True" value="' . $PropertyOptionValue . '">' . $PropertyOptionValue . '</option>';
 					} else {
 						echo '<option value="' . $PropertyOptionValue . '">' . $PropertyOptionValue . '</option>';
 					}
