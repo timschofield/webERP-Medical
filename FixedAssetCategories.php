a4664b1bd1d846f482714a2ad25b1188a49c3cea
Tim Schofield <tim@weberpafrica.com>
2011-07-26 20:56:05 +0100

    XHTML corrections to code

--- FixedAssetCategories.php
+++ FixedAssetCategories.php
@@ -235,7 +235,7 @@ if (isset($SelectedCategory) and !isset($_POST['submit'])) {
 		$_POST['CategoryID'] = '';
 	}
 	echo '<br /><table class="selection"><tr><td>' . _('Category Code') . ':</td>
-				 <td><input type="Text" name="CategoryID" size=7 maxlength=6 value="' . $_POST['CategoryID'] . '"></td></tr>';
+				 <td><input type="text" name="CategoryID" size="7" maxlength="6" value="' . $_POST['CategoryID'] . '"></td></tr>';
 }
 
 //SQL to poulate account selection boxes
@@ -264,7 +264,7 @@ if (!isset($_POST['CategoryDescription'])) {
 }
 
 echo '<tr><td>' . _('Category Description') . ':</td>
-			<td><input type="Text" name="CategoryDescription" size=22 maxlength=20 value="' . $_POST['CategoryDescription'] . '"></td></tr>';
+			<td><input type="text" name="CategoryDescription" size="22" maxlength="20" value="' . $_POST['CategoryDescription'] . '" /></td></tr>';
 
 echo '<tr><td>' . _('Fixed Asset Cost GL Code');
 
