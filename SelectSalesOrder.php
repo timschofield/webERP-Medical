34ac77f7419fe966c4b275762e48231aef699fe3
Tim Schofield <tim@weberpafrica.com>
2011-07-29 20:42:34 +0100

    XHTML corrections to code

--- SelectSalesOrder.php
+++ SelectSalesOrder.php
@@ -488,7 +488,7 @@ if (!isset($StockID)) {
 
 if (isset($StockItemsResult) and DB_num_rows($StockItemsResult)>0) {
 
-	echo '<table cellpadding=2 colspan=7 class="selection">';
+	echo '<table cellpadding="2" colspan="7" class="selection">';
 	$TableHeader = '<tr>
 				<th>' . _('Code') . '</th>
 				<th>' . _('Description') . '</th>
@@ -734,7 +734,7 @@ if (isset($StockItemsResult) and DB_num_rows($StockItemsResult)>0) {
 		$AuthResult=DB_query($AuthSQL,$db);
 		$AuthRow=DB_fetch_array($AuthResult);
 
-                echo '<table cellpadding=2 colspan=7 width=95% class="selection">';
+                echo '<table cellpadding="2" colspan="7" width="95%" class="selection">';
 
 		if (isset($_POST['Quotations']) AND $_POST['Quotations']=='Orders_Only'){
 			$tableheader = '<tr>
