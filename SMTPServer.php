2d168e12ada279f878a688e9ef65b17dbbf77e8e
Tim Schofield <tim@weberpafrica.com>
2011-07-29 11:31:11 +0100

    XHTML corrections to code

--- SMTPServer.php
+++ SMTPServer.php
@@ -37,11 +37,11 @@ echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">';
 echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 echo '<table class="selection">';
 echo '<tr><td>'._('Server Host Name').'</td>
-		<td><input type="text" name=host value='.$myrow['host'].'></td></tr>';
+		<td><input type="text" name="host" value="'.$myrow['host'].'" /></td></tr>';
 echo '<tr><td>'._('SMTP port').'</td>
-		<td><input type="text" name=port size=4 class="number" value='.$myrow['port'].'></td></tr>';
+		<td><input type="text" name="port" size="4" class="number" value="'.$myrow['port'].'" /></td></tr>';
 echo '<tr><td>'._('Helo Command').'</td>
-		<td><input type="text" name=heloaddress value='.$myrow['heloaddress'].'></td></tr>';
+		<td><input type="text" name="heloaddress" value="'.$myrow['heloaddress'].'" /></td></tr>';
 echo '<tr><td>'._('Authorisation Required').'</td><td>';
 echo '<select name=auth>';
 if ($myrow['auth']==1) {
@@ -53,12 +53,12 @@ if ($myrow['auth']==1) {
 }
 echo '</select></td></tr>';
 echo '<tr><td>'._('User Name').'</td>
-	<td><input type="text" name=username value='.$myrow['username'].'></td></tr>';
+	<td><input type="text" name=username value="'.$myrow['username'].'" /></td></tr>';
 echo '<tr><td>'._('Password').'</td>
-	<td><input type=password name=password value='.$myrow['password'].'></td></tr>';
+	<td><input type="password" name="password" value="'.$myrow['password'].'" /></td></tr>';
 echo '<tr><td>'._('Timeout (seconds)').'</td>
 	<td><input type="text" size=5 name=timeout class="number" value='.$myrow['timeout'].'></td></tr>';
-echo '<tr><td colspan=2><div class="centre"><input type="submit" name="submit" value="' . _('Update') . '"></div></td></tr>';
+echo '<tr><td colspan="2"><div class="centre"><input type="submit" name="submit" value="' . _('Update') . '"></div></td></tr>';
 echo '</table></form>';
 
 include('includes/footer.inc');
