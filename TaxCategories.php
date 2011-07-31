5302413dd7e11cf99da1c63a302fd4f9110cf83a
Tim Schofield <tim@weberpafrica.com>
2011-07-31 21:35:49 +0100

    XHTML corrections to code

--- TaxCategories.php
+++ TaxCategories.php
@@ -226,7 +226,7 @@ if (! isset($_GET['delete'])) {
 	}
 	echo '<tr>
 		<td>' . _('Tax Category Name') . ':' . '</td>
-		<td><input type="text" name="TaxCategoryName" size=30 maxlength=30 value="' . $_POST['TaxCategoryName'] . '"></td>
+		<td><input type="text" name="TaxCategoryName" size="30" maxlength="30" value="' . $_POST['TaxCategoryName'] . '" /></td>
 		</tr>';
 	echo '</table>';
 
