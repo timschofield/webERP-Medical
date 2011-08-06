01e439f17346e47fc13503a99e3f360aea1eae6d
Tim Schofield <tim@weberpafrica.com>
2011-08-06 19:59:58 +0100

    XHTML corrections to code

--- StockUsage.php
+++ StockUsage.php
@@ -26,8 +26,7 @@ if (isset($_POST['ShowGraphUsage'])) {
 
 include('includes/header.inc');
 
-echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/magnifier.png" title="' . _('Dispatch') .
-		'" alt="" />' . ' ' . $title . '</p>';
+echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/magnifier.png" title="' . _('Dispatch') . '" alt="" />' . ' ' . $title . '</p>';
 
 $result = DB_query("SELECT description,
 				units,
