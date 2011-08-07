04d1de17277b0d12a369231be2e96799213f317b
Tim Schofield <tim@weberpafrica.com>
2011-08-07 16:59:45 +0100

    XHTML corrections to code

--- BOMExtendedQty.php
+++ BOMExtendedQty.php
@@ -269,13 +269,12 @@ if (isset($_POST['PrintPDF'])) {
 
 	$title=_('Quantity Extended BOM Listing');
 	include('includes/header.inc');
-	echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' .
-		_('Search') . '" alt="" />' . ' ' . $title.'</p><br />';
+	echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' . _('Search') . '" alt="" />' . ' ' . $title.'</p><br />';
 
 	echo '<br /><br /><form action=' . $_SERVER['PHP_SELF'] . ' method="post"><table class="selection">';
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
-	echo '<tr><td>' . _('Part') . ':</td><td><input type ="text" name="Part" size="20">';
-	echo '<tr><td>' . _('Quantity') . ':</td><td><input type="text" class="number" name="Quantity" size="4">';
+	echo '<tr><td>' . _('Part') . ':</td><td><input type ="text" name="Part" size="20" />';
+	echo '<tr><td>' . _('Quantity') . ':</td><td><input type="text" class="number" name="Quantity" size="4" />';
 	echo '<tr><td>' . _('Selection Option') . ':</td><td><select name="Select">';
 	echo '<option selected="True" value="All">' . _('Show All Parts') . '</option>';
 	echo '<option value="Shortages">' . _('Only Show Shortages') . '</option>';
