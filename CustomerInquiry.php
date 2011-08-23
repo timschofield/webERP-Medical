353f1780219c5cd63fe63959101c55b519439532
Tim Schofield <tim@weberpafrica.com>
2011-08-23 11:37:54 +0100

    Change && to and, to comply with coding guidelines

--- CustomerInquiry.php
+++ CustomerInquiry.php
@@ -244,7 +244,7 @@ while ($myrow=DB_fetch_array($TransResult)) {
 	<td><a href="%s/EmailCustTrans.php?FromTransNo=%s&InvOrCredit=Credit">' . _('Email') . ' <img src="%s/email.gif" title="' . _('Click to email the credit note') . '" /></a></td>';
 
 	/* assumed allowed page security token 3 allows the user to create credits for invoices */
-	if (in_array(3,$_SESSION['AllowedPageSecurityTokens']) && $myrow['type']==10){
+	if (in_array(3,$_SESSION['AllowedPageSecurityTokens']) and $myrow['type']==10){
 		/*Show a link to allow an invoice to be credited */
 
 		/* assumed allowed page security token 8 allows the user to see GL transaction information */
