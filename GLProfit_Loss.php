ad323c726aa3186c14bb1ffd074c6c08c8f4bd96
Tim Schofield <tim@weberpafrica.com>
2011-08-28 14:27:55 +0100

    XHTML corrections

--- GLProfit_Loss.php
+++ GLProfit_Loss.php
@@ -534,7 +534,7 @@ if ((!isset($_POST['FromPeriod']) AND !isset($_POST['ToPeriod'])) OR isset($_POS
 } else {
 
 	include('includes/header.inc');
-	echo '<form method="POST" action="' . $_SERVER['PHP_SELF'] . '">';
+	echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">';
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 	echo '<input type="hidden" name="FromPeriod" value="' . $_POST['FromPeriod'] . '" />
 			<input type="hidden" name="ToPeriod" value="' . $_POST['ToPeriod'] . '" />';
