7fc21a1f46120bfea2cb25b902f9a6d629ea4bdf
Tim Schofield <tim@weberpafrica.com>
2011-08-28 13:50:20 +0100

    XHTML corrections

--- SuppPaymentRun.php
+++ SuppPaymentRun.php
@@ -240,7 +240,7 @@ if ((isset($_POST['PrintPDF']) OR isset($_POST['PrintPDFAndProcess']))
 
 	/* show form to allow input	*/
 
-	echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="POST"><table class="selection">';
+	echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="post"><table class="selection">';
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
 
 	if (!isset($_POST['FromCriteria']) or mb_strlen($_POST['FromCriteria'])<1){
