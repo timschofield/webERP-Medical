e5f2deffcd2320be68352d3b6a570ee6023ce16e
Tim Schofield <tim@weberpafrica.com>
2011-10-03 16:33:38 +0100

    Remove all SELECT * references, only choose those fields that are needed

--- sql/mysql/updates/80.php
+++ sql/mysql/updates/80.php
@@ -2,7 +2,7 @@
 
 InsertRecord('securitytokens',array('tokenid'),array('0'),array('tokenid, tokenname'), array('0', 'Main Index Page'), $db);
 
-$result=DB_query("SELECT * FROM securitygroups WHERE tokenid='0'", $db);
+$result=DB_query("SELECT secroleid FROM securitygroups WHERE tokenid='0'", $db);
 
 if (DB_num_rows($result)==0) {
 	DB_query("INSERT INTO securitygroups (SELECT secroleid, '0' FROM securityroles)" , $db);
