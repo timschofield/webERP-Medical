c05d3054c497d766456e433a474e8f4a100c2ea8
Tim Schofield <tim@weberpafrica.com>
2012-04-26 18:30:21 +0100

    Layout improvements

--- Z_UploadForm.php
+++ Z_UploadForm.php
@@ -7,13 +7,21 @@ $title=_('File Upload');
 
 include('includes/header.inc');
 
-echo '<form enctype="multipart/form-data" action="Z_UploadResult.php" method="post">';
-	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+echo '<p class="page_title_text"><img src="'.$rootpath.'/css/'.$theme.'/images/maintenance.png" title="' . _('Search') . '" alt="" />' . ' ' . $title.'</p><br />';
 
-echo '<input type="hidden" name="MAX_FILE_SIZE" value="1000000" />' .
-		_('Send this file') . ': <input name="userfile" type="file" />
-		<input type="submit" value="' . _('Send File') . '" />
-		</form>';
+echo '<form enctype="multipart/form-data" action="Z_UploadResult.php" method="post">';
+echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+
+echo '<input type="hidden" name="MAX_FILE_SIZE" value="1000000" />';
+echo '<table class="selection">
+		<tr>
+			<td>' . _('Send this file') . ': </td>
+			<td><input name="userfile" type="file" /></td>
+		</tr>
+		<tr>
+			<td colspan="2"><div class="centre"><button type="submit">' . _('Send File') . '</button></div></td>
+		</tr>
+	</table></form>';
 
 include('includes/footer.inc');
 ?>
