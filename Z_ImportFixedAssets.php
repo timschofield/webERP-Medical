7bc74a40387958ab46b7a53bb6825f01f7c25cb5
Tim Schofield <tim@weberpafrica.com>
2012-04-27 01:28:47 +0100

    Layout improvements, new css and buttons

--- Z_ImportFixedAssets.php
+++ Z_ImportFixedAssets.php
@@ -301,11 +301,10 @@ if ($_FILES['SelectedAssetFile']['name']) { //start file processing
 	}
 	echo '</select></td></tr>';
 	echo '<tr><td>' . _('Fixed Assets Upload file:') . '</td><td><input name="SelectedAssetFile" type="file" /></tr></table>
-			<input type="submit" value="' . _('Send File') . '" />
+			<button type="submit">' . _('Send File') . '</button>
 		</form>';
 
 }
-
 
 include('includes/footer.inc');
 ?>
