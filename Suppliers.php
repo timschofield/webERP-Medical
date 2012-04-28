2de3e139a0d5b680d21c7946d4e6292c16e2a92a
Tim Schofield <tim@weberpafrica.com>
2012-04-28 22:14:16 +0100

    Layout improvements, new css and buttons

--- Suppliers.php
+++ Suppliers.php
@@ -654,7 +654,7 @@ if (!isset($SupplierID)) {
 	echo '<tr><td>' . _('Address Line 4 (Postal Code)') . ':</td><td><input type="text" name="Address4" size="42" maxlength="40" /></td></tr>';
 	echo '<tr><td>' . _('Telephone') . ':</td><td><input type="text" name="Phone" size="25" maxlength="25" /></td></tr>';
 	echo '<tr><td>' . _('Facsimile') . ':</td><td><input type="text" name="Fax" size="25" maxlength="25" /></td></tr>';
-	echo '<tr><td>' . _('Email Address') . ':</td><td><input class="email" type="text" name="Email" size="30" maxlength="40" /></td></tr>';
+	echo '<tr><td>' . _('Email Address') . ':</td><td><input class="email" type="email" name="Email" size="30" maxlength="40" /></td></tr>';
 	echo '<tr><td>' . _('Supplier Type') . ':</td><td><select name="SupplierType">';
 	$result=DB_query("SELECT typeid, typename FROM suppliertype", $db);
 	while ($myrow = DB_fetch_array($result)) {
@@ -730,7 +730,7 @@ if (!isset($SupplierID)) {
 		}
 	} //end while loop
 
-	echo '</select></td></tr></table><br /><div class="centre"><input type="submit" name="submit" value="' . _('Insert New Supplier') . '" />';
+	echo '</select></td></tr></table><br /><div class="centre"><button type="submit" name="submit">' . _('Insert New Supplier') . '</button>';
 	echo '</div></form>';
 
 } else {
@@ -932,12 +932,12 @@ if (!isset($SupplierID)) {
 	echo '</select></td></tr></table>';
 
 	if (isset($_POST['New'])) {
-		echo '<br /><div class="centre"><input type="submit" name="submit" value="' . _('Add These New Supplier Details') . '" /></div></form>';
+		echo '<br /><div class="centre"><button type="submit" name="submit">' . _('Add These New Supplier Details') . '</button></div></form>';
 	} else {
-		echo '<br /><div class="centre"><input type="submit" name="submit" value="' . _('Update Supplier') . '" /></div><br />';
+		echo '<br /><div class="centre"><button type="submit" name="submit">' . _('Update Supplier') . '</button></div><br />';
 //		echo '<font color=red><b>' . _('WARNING') . ': ' . _('There is no second warning if you hit the delete button below') . '. ' . _('However checks will be made to ensure there are no outstanding purchase orders or existing accounts payable transactions before the deletion is processed') . '<br /></font></b>';
 		prnMsg(_('WARNING') . ': ' . _('There is no second warning if you hit the delete button below') . '. ' . _('However checks will be made to ensure there are no outstanding purchase orders or existing accounts payable transactions before the deletion is processed'), 'Warn');
-		echo '<br /><div class="centre"><input type="submit" name="delete" value="' . _('Delete Supplier') . '" onclick="return confirm(' . _('Are you sure you wish to delete this supplier?') . ');" />';
+		echo '<br /><div class="centre"><button type="submit" name="delete" onclick="return confirm(' . _('Are you sure you wish to delete this supplier?') . ');">' . _('Delete Supplier') . '</button>';
 		echo '<br /><a href="' . $rootpath . '/SupplierContacts.php?SupplierID=' . $SupplierID . '">' . _('Review Contact Details') . '</a></div></form>';
 	}
 } // end of main ifs
