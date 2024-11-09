cc8c63e3d766c11d254f3c88b5384619dc05097a
Tim Schofield <tim.schofield1960@googlemail.com>
2024-11-09 11:42:57 +0000

    Javascript functions used for drawing tabbed pages

---
+++ javascripts/tabs.js
@@ -0,0 +1,25 @@
+function openPage(pageName, elmnt) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+	tablinks[i].style.color = "white";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = "#FAFA53";
+  elmnt.style.color = "black";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
